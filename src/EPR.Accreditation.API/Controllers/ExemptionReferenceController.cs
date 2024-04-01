9e77280a83374d861b2d8d214cd3639e71c573b9
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-01 12:30:34 +0100

    [feature/336347: Initial controller and DTO creation with the GET endpoint]

---
+++ src/EPR.Accreditation.API/Controllers/ExemptionReferenceController.cs
@@ -0,0 +1,30 @@
+﻿using EPR.Accreditation.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using DTO = EPR.Accreditation.API.Common.Dtos;
+
+namespace EPR.Accreditation.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]/{id}")]
+    public class ExemptionReferenceController : ControllerBase
+    {
+        protected readonly IAccreditationService _accreditationService;
+
+        public ExemptionReferenceController(IAccreditationService accreditationService)
+        {
+            _accreditationService = accreditationService ?? throw new ArgumentNullException(nameof(accreditationService));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(DTO.ExemptionReference), 200)]
+        public async Task<IActionResult> GetExemptionReference(Guid id)
+        {
+            var exemptionReference = await _accreditationService.GetExemptionReference(id);
+
+            if (exemptionReference == null)
+                return NotFound();
+
+            return Ok(exemptionReference);
+        }
+    }
+}
