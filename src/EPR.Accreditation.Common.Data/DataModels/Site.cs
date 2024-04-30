e15836f1391bc3f200c0233073431abe69a32097
Nicholas Peters <nicholas.peters.external@eviden.com>
2024-04-30 16:04:40 +0100

    Removed commented out code

--- src/EPR.Accreditation.Common.Data/DataModels/Site.cs
+++ src/EPR.Accreditation.Common.Data/DataModels/Site.cs
@@ -9,26 +9,6 @@
         // so this field will be required
         public Guid ExternalId { get; set; } // unique key created in DbContext
 
-        //[Required]
-        //[MaxLength(100)]
-        //public string Address1 { get; set; }
-
-        //[MaxLength(100)]
-        //public string Address2 { get; set; }
-
-        //[Required]
-        //[MaxLength(100)]
-        //public string Town { get; set; }
-
-        //[MaxLength(100)]
-        //public string County { get; set; }
-
-        //[Required]
-        //[MaxLength(10)]
-        //public string Postcode { get; set; } // along with OrganisationId this is part of a compound unique key
-
-        //public Guid OrganisationId { get; set; } // along with Postcode this is part of a compound unique key
-
         [ForeignKey("Address")]
         public int AddressId { get; set; }
 
