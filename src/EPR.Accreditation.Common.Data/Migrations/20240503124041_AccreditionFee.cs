c93d37ab25529c1f5935118f43ef4aa7b9a682b7
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-05-03 13:43:25 +0100

    Latest changes.

---
+++ src/EPR.Accreditation.Common.Data/Migrations/20240503124041_AccreditionFee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Accreditation.API.Common.Data.Migrations
+{
+    public partial class AccreditionFee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AccreditationFee",
+                table: "Accreditation",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccreditationFee",
+                table: "Accreditation");
+        }
+    }
+}
