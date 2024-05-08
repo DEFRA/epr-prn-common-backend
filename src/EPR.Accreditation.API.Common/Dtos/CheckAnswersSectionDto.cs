015854d673ce719699f324207cf433726b69d1f6
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-05-08 11:11:58 +0100

    [feature/346562-new: Removed commented out code]

--- src/EPR.Accreditation.API.Common/Dtos/CheckAnswersSectionDto.cs
+++ src/EPR.Accreditation.API.Common/Dtos/CheckAnswersSectionDto.cs
@@ -2,14 +2,10 @@
 {
     public class CheckAnswersSectionDto
     {
-        //public Guid Id { get; set; }
-
         public string Title { get; set; }
 
         public bool Completed { get; set; }
 
         public List<CheckAnswersSectionRow> SectionRows { get; set; }
-
-        //public Dictionary<string, string> QueryStringRouteData { get; set; } = new();
     }
 }
