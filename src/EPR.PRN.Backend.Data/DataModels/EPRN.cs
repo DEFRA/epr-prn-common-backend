77621fc4fd074adddde1e294f6d98c88333f864c
Alex Joyce <alex.joyce.external@eviden.com>
2024-07-17 15:43:19 +0100

    Initialise start up, db components and EF models

---
+++ src/EPR.PRN.Backend.Data/DataModels/EPRN.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EPR.PRN.Backend.Data.DataModels
+{
+	public class EPRN
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[Required]
+		public Guid ExternalId { get; set; }
+		
+		[MaxLength(20)]
+		public required string PrnNumber { get; set; }
+		
+		[Required]
+		public Guid OrganisationId { get; set; }
+		
+		[Required, MaxLength(50)]
+		public string OrganisationName { get; set; }
+		
+		[MaxLength(50)]
+		public string ProducerAgency { get; set; }
+		
+		[MaxLength(50)]
+		public string ReprocessorExporterAgency { get; set; }
+		
+		public int PrnStatusId { get; set; }
+		
+		public int TonnageValue { get; set; }
+		
+		[MaxLength(20)]
+		public required string MaterialName { get; set; }
+		
+		[MaxLength(200)]
+		public string? IssuerNotes { get; set; }
+		
+		[MaxLength(200)]
+		public string IssuerReference { get; set; }
+		
+		[MaxLength(50)]
+		public string? PrnSignatory { get; set; }
+		
+		[MaxLength(50)]
+		public string? PrnSignatoryPosition { get; set; }
+		
+		[MaxLength(100)]
+		public string? Signature { get; set; }
+		
+		public DateTime IssueDate { get; set; }
+		
+		[MaxLength(20)]
+		public string? ProcessToBeUsed { get; set; }
+		
+		public bool DecemberWaste { get; set; }
+		
+		public DateTime? CancelledDate { get; set; }
+		
+		[MaxLength(50)]
+		public string IssuedByOrg { get; set; }
+		
+		[MaxLength(20)]
+		public string AccreditationNumber { get; set; }
+		
+		[MaxLength(100)]
+		public string? ReprocessingSite { get; set; }
+		
+		[MaxLength(10)]
+		public string AccreditationYear { get; set; }
+		
+		[MaxLength(10)]
+		public string ObligationYear { get; set; }
+		
+		[MaxLength(100)]
+		public string PackagingProducer { get; set; }
+		
+		[MaxLength(20)]
+		public string? CreatedBy { get; set; }
+		
+		public DateTime CreatedOn { get; set; }
+		
+		[Required]
+		public Guid LastUpdatedBy { get; set; }
+		
+		public DateTime LastUpdatedDate { get; set; }
+		
+		public bool IsExport { get; set; }
+	}
+}
---
+++ src/EPR.PRN.Backend.Data/EprContext.cs
@@ -0,0 +1,40 @@
+﻿using EPR.PRN.Backend.Data.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPR.PRN.Backend.Data
+{
+	public class EprContext : DbContext
+	{
+		public EprContext()
+		{
+		}
+
+		public EprContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<EPRN>()
+				.HasIndex(a => a.ExternalId)
+				.IsUnique();
+
+			modelBuilder.Entity<EPRN>()
+				.HasIndex(a => a.PrnNumber)
+				.IsUnique();
+			
+			base.OnModelCreating(modelBuilder);
+		}
+
+		public virtual DbSet<EPRN> Prn { get; set; }
+		
+		public virtual DbSet<PrnStatus> PrnStatus { get; set; }
+
+		public virtual DbSet<PrnStatusHistory> PrnStatusHistory { get; set; }
+	}
+}
