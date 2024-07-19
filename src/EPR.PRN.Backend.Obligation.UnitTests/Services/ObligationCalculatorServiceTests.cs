2a6300f5455a9ae9e4baf005ecc8a3225a01b924
Steven Moschidis <steven.moschidis.external@eviden.com>
2024-07-19 16:23:15 +0100

    initial commit

---
+++ src/EPR.PRN.Backend.Obligation.UnitTests/Services/ObligationCalculatorServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using EPR.PRN.Backend.Obligation.Services;
+using FluentAssertions;
+
+namespace EPR.PRN.Backend.Obligation.UnitTests.Services
+{
+    [TestClass]
+    public class ObligationCalculatorServiceTests
+    {
+        [TestMethod]
+        [DataRow(0.61, 100, 61)]
+        [DataRow(0.61, 120, 73)]
+        [DataRow(0.6, 128, 77)]
+        public void CalculateObligation_WhenAluminium_ThenObligationIsReturned(double target, int tonnage, int expected)
+        {
+            var obligationCalculator = new ObligationCalculatorService();
+
+            var result = obligationCalculator.Calculate(target, tonnage);
+
+            result.Should().Be(expected, "the expected target should match the calculated obligation");
+        }
+    }
+}
---
+++ src/EPR.PRN.Backend.Obligation/Services/ObligationCalculatorService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EPR.PRN.Backend.Obligation.Services
+{
+    public class ObligationCalculatorService
+    {
+        public int Calculate(double target, int tonnage)
+        {
+            return (int)Math.Round(target * tonnage, 0, MidpointRounding.AwayFromZero);
+        }
+    }
+}
