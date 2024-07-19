e6778e20e1ea80a758ae4f0c5e9790335de4d4c3
himatvarsani-atos <156190828+himatvarsani-atos@users.noreply.github.com>
2024-07-19 16:35:43 +0100

    Feature/epr prn common backend pipeline (#78)
    * Pipeline creation for epr-prn-common-bakend pipeline
    
    * Initialise start up, db components and EF models
    
    * create migrations
    
    * constructor
    
    * context
    
    * app settings
    
    * conn string
    
    * Database backend build
    
    * remove NuGet run
    
    * remove NuGet run
    
    * Database backend build
    
    * Update EPR.PRN.Backend.Data.csproj
    
    * Update EPR.PRN.Backend.Data.csproj
    
    * Update EPR.PRN.Backend.Data.csproj
    
    * Update Initialise-SQL-db.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * Update Initialise-SQL-db.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    ---------
    
    Co-authored-by: Himat Varsani <himat.varsani@eviden.com>
    Co-authored-by: Alex Joyce <alex.joyce.external@eviden.com>

---
+++ src/EPR.PRN.Backend.API/Startup.cs
@@ -0,0 +1,47 @@
+﻿using EPR.PRN.Backend.Data;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPR.PRN.Backend.API
+{
+	[ExcludeFromCodeCoverage]
+	public class Startup
+	{
+		private readonly IConfiguration _config;
+
+		public Startup(IHostEnvironment env, IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen(config =>
+			{
+				config.CustomSchemaIds(s => s.FullName);
+			});
+			services.AddDbContext<EprContext>(options =>
+				options.UseSqlServer(_config.GetConnectionString("EprnConnectionString"))
+			);
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseRouting();
+			app.UseAuthorization();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
+
---
+++ src/EPR.PRN.Backend.Data/Scripts/migrations.sql
@@ -0,0 +1,117 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Prn] (
+        [Id] int NOT NULL IDENTITY,
+        [ExternalId] uniqueidentifier NOT NULL,
+        [PrnNumber] nvarchar(20) NOT NULL,
+        [OrganisationId] uniqueidentifier NOT NULL,
+        [OrganisationName] nvarchar(50) NOT NULL,
+        [ProducerAgency] nvarchar(50) NOT NULL,
+        [ReprocessorExporterAgency] nvarchar(50) NOT NULL,
+        [PrnStatusId] int NOT NULL,
+        [TonnageValue] int NOT NULL,
+        [MaterialName] nvarchar(20) NOT NULL,
+        [IssuerNotes] nvarchar(200) NULL,
+        [IssuerReference] nvarchar(200) NOT NULL,
+        [PrnSignatory] nvarchar(50) NULL,
+        [PrnSignatoryPosition] nvarchar(50) NULL,
+        [Signature] nvarchar(100) NULL,
+        [IssueDate] datetime2 NOT NULL,
+        [ProcessToBeUsed] nvarchar(20) NULL,
+        [DecemberWaste] bit NOT NULL,
+        [CancelledDate] datetime2 NULL,
+        [IssuedByOrg] nvarchar(50) NOT NULL,
+        [AccreditationNumber] nvarchar(20) NOT NULL,
+        [ReprocessingSite] nvarchar(100) NULL,
+        [AccreditationYear] nvarchar(10) NOT NULL,
+        [ObligationYear] nvarchar(10) NOT NULL,
+        [PackagingProducer] nvarchar(100) NOT NULL,
+        [CreatedBy] nvarchar(20) NULL,
+        [CreatedOn] datetime2 NOT NULL,
+        [LastUpdatedBy] uniqueidentifier NOT NULL,
+        [LastUpdatedDate] datetime2 NOT NULL,
+        [IsExport] bit NOT NULL,
+        CONSTRAINT [PK_Prn] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [PrnStatus] (
+        [Id] int NOT NULL IDENTITY,
+        [StatusName] nvarchar(20) NOT NULL,
+        [StatusDescription] nvarchar(50) NULL,
+        CONSTRAINT [PK_PrnStatus] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [PrnStatusHistory] (
+        [Id] int NOT NULL IDENTITY,
+        [CreatedOn] datetime2 NOT NULL,
+        [CreatedByUser] uniqueidentifier NOT NULL,
+        [CreatedByOrganisationId] uniqueidentifier NOT NULL,
+        [PrnStatusIdFk] int NOT NULL,
+        [PrnIdFk] int NOT NULL,
+        [Comment] nvarchar(1000) NULL,
+        CONSTRAINT [PK_PrnStatusHistory] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Prn_ExternalId] ON [Prn] ([ExternalId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Prn_PrnNumber] ON [Prn] ([PrnNumber]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240717145045_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240717145045_InitialCreate', N'8.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
