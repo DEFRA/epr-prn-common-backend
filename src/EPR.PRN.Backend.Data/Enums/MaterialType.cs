a01f9a6da7a49d4de2ce57dd6d7bdd786ac136fa
Saywa Singh Boparai <saywa.boparai@eviden.com>
2024-07-30 16:07:02 +0100

    415911 Adds a RecyclingTargetRepository

---
+++ src/EPR.PRN.Backend.Data/Enums/MaterialType.cs
@@ -0,0 +1,13 @@
+﻿namespace EPR.PRN.Backend.Data.Enums
+{
+    public enum MaterialType
+    {
+        None = 0,
+        Paper,
+        Glass,
+        Aluminium,
+        Steel,
+        Plastic,
+        Wood
+    }
+}
