dff1c00bfe37af0a47e9682891107ebe10583a16
Saywa Singh Boparai <saywa.boparai@eviden.com>
2024-08-06 10:06:19 +0100

    422135 Create ObligationCalculation table

---
+++ src/EPR.PRN.Backend.Data/DataModels/ObligationCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EPR.PRN.Backend.Data.DataModels
+{
+    public class ObligationCalculation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrganisationId { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string MaterialName { get; set; }
+
+        [Required]
+        public int MaterialObligationValue { get; set; }
+
+        [Required]
+        public DateTime CalculatedOn { get; set; }
+    }
+}
