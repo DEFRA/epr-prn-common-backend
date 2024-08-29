5ff11ef810dc25dde69a22339a9c93cbe4d973c6
taseakpoexternal <tase.akpo.external@eviden.com>
2024-08-29 09:18:43 +0100

    Remove accessing common data api from this project as Common data api will be accessed by the azure function (#96)

--- src/EPR.PRN.Backend.Obligation/Services/ObligationCalculatorService.cs
+++ src/EPR.PRN.Backend.Obligation/Services/ObligationCalculatorService.cs
@@ -1,8 +1,6 @@
-﻿using System.Net.Http.Json;
-using EPR.PRN.Backend.Data.DataModels;
+﻿using EPR.PRN.Backend.Data.DataModels;
 using EPR.PRN.Backend.Data.Interfaces;
 using EPR.PRN.Backend.Obligation.DTO;
-using EPR.PRN.Backend.Obligation.Enums;
 using EPR.PRN.Backend.Obligation.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -13,64 +11,46 @@ namespace EPR.PRN.Backend.Obligation.Services
         private readonly ILogger<ObligationCalculatorService> _logger;
         private readonly IRecyclingTargetDataService _recyclingTargetDataService;
         private readonly IObligationCalculationRepository _obligationCalculationRepository;
-        private readonly IPomSubmissionData _pomSubmissionData;
         private readonly IMaterialCalculationStrategyResolver _strategyResolver;
 
         public ObligationCalculatorService(
             ILogger<ObligationCalculatorService> logger,
             IRecyclingTargetDataService recyclingTargetDataService,
             IObligationCalculationRepository obligationCalculationRepository,
-            IPomSubmissionData pomSubmissionData,
             IMaterialCalculationStrategyResolver strategyResolver)
         {
             _logger = logger;
             _recyclingTargetDataService = recyclingTargetDataService;
             _obligationCalculationRepository = obligationCalculationRepository;
-            _pomSubmissionData = pomSubmissionData;
             _strategyResolver = strategyResolver;
         }
 
         public async Task ProcessApprovedPomData(string submissionIdString)
         {
-            //Worker Service will call this method peridoically 
-            var response = await _pomSubmissionData.GetAggregatedPomData(submissionIdString);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _logger.LogError("Could not retrieve POM data for Submission Id: {0}.", submissionIdString);
-                return;
-            }
-
-            var pomData = response.Content.ReadFromJsonAsync<List<PomObligtionDto>>().Result;
-
-            if (pomData == null || pomData.Count == 0)
-            {
-                _logger.LogError("No POM data returned for Submission Id: {0}.", submissionIdString);
-                return;
-            }
+            //Pom Data will be provided by the function app request - still work in progress
 
             var recyclingTargets = await _recyclingTargetDataService.GetRecyclingTargetsAsync();
 
             var calculations = new List<ObligationCalculation>();
 
-            foreach (var material in pomData)
-            {
-                Enum.TryParse<MaterialType>(material.PackagingMaterial, out var materialType);
-                var strategy = _strategyResolver.Resolve(materialType);
+            //foreach (var material in pomData)
+            //{
+            //    Enum.TryParse<MaterialType>(material.PackagingMaterial, out var materialType);
+            //    var strategy = _strategyResolver.Resolve(materialType);
 
-                if (strategy == null)
-                {
-                    _logger.LogError("Skipping material with unknown type: {0} for SubmissionId: {1}.", materialType, submissionIdString);
-                    continue;
-                }
-                calculations.AddRange(strategy.Calculate(material, materialType, recyclingTargets));
-            }
+            //    if (strategy == null)
+            //    {
+            //        _logger.LogError("Skipping material with unknown type: {0} for SubmissionId: {1}.", materialType, submissionIdString);
+            //        continue;
+            //    }
+            //    calculations.AddRange(strategy.Calculate(material, materialType, recyclingTargets));
+            //}
 
-            if (calculations.Count <= 0)
-            {
-                _logger.LogError("No calculations were saved for SubmissionId: {0}.", submissionIdString);
-                return;
-            }
+            //if (calculations.Count <= 0)
+            //{
+            //    _logger.LogError("No calculations were saved for SubmissionId: {0}.", submissionIdString);
+            //    return;
+            //}
 
             await _obligationCalculationRepository.AddObligationCalculation(calculations);
         }
