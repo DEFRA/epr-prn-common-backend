9e035dac5242e731a7e40761c52322659a854bc6
Tase Akpo <tase.akpo.external@eviden.com>
2024-10-11 08:52:36 +0100

    fix migration and merge sprint branch

--- src/EPR.PRN.Backend.Data/Scripts/migrations.sql
+++ src/EPR.PRN.Backend.Data/Scripts/migrations.sql
@@ -240,31 +240,6 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20240924171141_AddMaterialWeight'
-)
-BEGIN
-    ALTER TABLE [ObligationCalculations] ADD [MaterialWeight] float NOT NULL DEFAULT 0.0E0;
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20240924171141_AddMaterialWeight'
-)
-BEGIN
-    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20240924171141_AddMaterialWeight', N'8.0.8');
-END;
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
     WHERE [MigrationId] = N'20240924172133_UpdateEprnStatusCancelledCorrection'
 )
 BEGIN
