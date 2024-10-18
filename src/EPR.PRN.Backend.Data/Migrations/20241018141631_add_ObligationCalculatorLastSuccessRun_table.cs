f72a49057747152fd6c0b3603d9b414b09f5e3aa
harishpaladugu13 <harish.paladugu.external@eviden.com>
2024-10-18 16:26:52 +0100

    add migration scripts

---
+++ src/EPR.PRN.Backend.Data/Migrations/20241018141631_add_ObligationCalculatorLastSuccessRun_table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.PRN.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_ObligationCalculatorLastSuccessRun_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ObligationCalculatorLastSuccessRun",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastSuccessfulRunDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObligationCalculatorLastSuccessRun", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ObligationCalculatorLastSuccessRun");
+        }
+    }
+}
