f48fd11680d517ebe5f1ae18f743d8113fbb3061
Tase Akpo <tase.akpo.external@eviden.com>
2024-11-18 09:59:03 +0000

    Add additional test

--- src/EPR.PRN.Backend.Data/Repositories/PrnRepository.cs
+++ src/EPR.PRN.Backend.Data/Repositories/PrnRepository.cs
@@ -6,16 +6,16 @@ namespace EPR.PRN.Backend.Data.Repositories
 {
     public class PrnRepository(EprContext context) : IPrnRepository
     {
-        public IQueryable<EprnResultsDto> GetAcceptedAndAwaitingPrnsByYear(Guid organisationId)
+        public IQueryable<EprnResultsDto> GetAcceptedAndAwaitingPrnsByYear(Guid organisationId, int year)
         {
             return context.Prn.Join(
-                        context.PrnStatus,
-                        eprn => eprn.PrnStatusId,
-                        status => status.Id,
-                        (eprn, status) => new EprnResultsDto { Eprn = eprn, Status = status }
+                    context.PrnStatus,
+                    eprn => eprn.PrnStatusId,
+                    status => status.Id,
+                    (eprn, status) => new EprnResultsDto { Eprn = eprn, Status = status }
                     )
                     .Where(joined => joined.Eprn.OrganisationId == organisationId && (joined.Status.StatusName == EprnStatus.ACCEPTED.ToString()
-                    || joined.Status.StatusName == EprnStatus.AWAITINGACCEPTANCE.ToString()));
+                    || joined.Status.StatusName == EprnStatus.AWAITINGACCEPTANCE.ToString()) && joined.Eprn.ObligationYear == year.ToString());
         }
     }
 }
