4a58f98bc66cce900c19e995475c23cd57e19055
Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>
2024-12-05 19:18:29 +0000

    Added npwd status sync table

--- src/EPR.PRN.Backend.API/Controllers/PrnController.cs
+++ src/EPR.PRN.Backend.API/Controllers/PrnController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System.Net;
+using System.Security.Cryptography;
 
 [ApiController]
 [ApiVersion("1.0")]
