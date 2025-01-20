a2ac3a378a2c7e217e282568eedd454cac281f2e
MasilEviden <masil.puveendran@eviden.com>
2025-01-20 11:35:10 +0000

    Fixed typo error for 'UpdatedOn'

--- src/EPR.PRN.Backend.Data/DataModels/PrnStatusSync.cs
+++ src/EPR.PRN.Backend.Data/DataModels/PrnStatusSync.cs
@@ -9,7 +9,7 @@ namespace EPR.PRN.Backend.Data.DataModels
     public class PrnStatusSync
     {
         public string StatusName { get; set; } = string.Empty;
-        public DateTime? UpatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
         public string OrganisationName { get; set; } = string.Empty;
         public string PrnNumber { get; set; } = string.Empty;
     }
