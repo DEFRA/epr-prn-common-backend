5fc732f54272679348ebc8f1c8c2047b33c0d0e5
harishpaladugu13 <harish.paladugu.external@eviden.com>
2025-03-06 12:09:04 +0000

    revert back blank space change

--- src/EPR.PRN.Backend.API/Models/InsertSyncedPrns.cs
+++ src/EPR.PRN.Backend.API/Models/InsertSyncedPrns.cs
@@ -9,7 +9,7 @@ namespace EPR.PRN.Backend.API.Models
     {
         public string EvidenceNo { get; set; } = null!;
 
-        [AllowedValues(EprnStatus.ACCEPTED, EprnStatus.REJECTED,ErrorMessage = "Only 'ACCEPTED' or 'REJECTED' status are allowed")]
+        [AllowedValues(EprnStatus.ACCEPTED,EprnStatus.REJECTED, ErrorMessage = "Only 'ACCEPTED' or 'REJECTED' status are allowed")]
         public EprnStatus EvidenceStatus { get; set; }
     }
 }
