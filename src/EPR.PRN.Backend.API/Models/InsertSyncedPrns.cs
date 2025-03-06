fe22a1d965238624a4a6fff5f62d4541ef7401e0
harishpaladugu13 <harish.paladugu.external@eviden.com>
2025-03-06 12:08:11 +0000

    Revert back model changes

--- src/EPR.PRN.Backend.API/Models/InsertSyncedPrns.cs
+++ src/EPR.PRN.Backend.API/Models/InsertSyncedPrns.cs
@@ -1,13 +1,15 @@
 ﻿using EPR.PRN.Backend.API.Common.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EPR.PRN.Backend.API.Models
 {
+    [ExcludeFromCodeCoverage]
     public class InsertSyncedPrn
     {
         public string EvidenceNo { get; set; } = null!;
 
-        [AllowedValues(EprnStatus.ACCEPTED, EprnStatus.REJECTED, ErrorMessage = "Only 'ACCEPTED' or 'REJECTED' status are allowed")]
+        [AllowedValues(EprnStatus.ACCEPTED, EprnStatus.REJECTED,ErrorMessage = "Only 'ACCEPTED' or 'REJECTED' status are allowed")]
         public EprnStatus EvidenceStatus { get; set; }
     }
 }
