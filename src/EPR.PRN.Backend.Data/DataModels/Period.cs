2a351b21503e517a6ca4648a001224c921549833
Shuyad Ali (ext) <shuyad.ali.external@eviden.com>
2025-03-10 10:18:30 +0000

    update

--- src/EPR.PRN.Backend.Data/DataModels/Period.cs
+++ src/EPR.PRN.Backend.Data/DataModels/Period.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EPR.PRN.Backend.Data.DataModels
 {
@@ -10,5 +11,23 @@ namespace EPR.PRN.Backend.Data.DataModels
         [MaxLength(100)]
         [Required]
         public string Name { get; set; }
+
+        [InverseProperty(nameof(RegistrationMaterial.MaxPeriod))]
+        public virtual ICollection<RegistrationMaterial> MaxPeriods { get; set; } = null!;
+
+        [InverseProperty(nameof(RegistrationMaterial.InstallationPeriod))]
+        public virtual ICollection<RegistrationMaterial> InstallationPeriods { get; set; } = null!;
+
+        [InverseProperty(nameof(RegistrationMaterial.EnvironmentalPermitWasteManagementPeriod))]
+        public virtual ICollection<RegistrationMaterial> EnvironmentalPermitWasteManagementPeriods { get; set; } = null!;
+
+        [InverseProperty(nameof(RegistrationMaterial.WasteManagementPeriod))]
+        public virtual ICollection<RegistrationMaterial> WasteManagementPeriods { get; set; } = null!;
+
+        [InverseProperty(nameof(RegistrationMaterial.PPCPeriod))]
+        public virtual ICollection<RegistrationMaterial> PPCPeriods { get; set; } = null!;
+
+        [InverseProperty(nameof(RegistrationMaterial.MaximumReprocessingPeriod))]
+        public virtual ICollection<RegistrationMaterial> MaximumReprocessingPeriods { get; set; } = null!;
     }
 }
