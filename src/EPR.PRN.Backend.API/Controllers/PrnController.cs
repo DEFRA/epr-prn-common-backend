899f462bddf76922f4e73d66654b1bbf64342f77
harishpaladugu13 <harish.paladugu.external@eviden.com>
2025-03-21 14:58:01 +0000

    remove organisation ids check

--- src/EPR.PRN.Backend.API/Controllers/PrnController.cs
+++ src/EPR.PRN.Backend.API/Controllers/PrnController.cs
@@ -126,11 +126,11 @@ public class PrnController(IPrnService prnService,
         logger.LogInformation("{Logprefix}: PrnController - GetObligationCalculation: Api Route api/v1/prn/obligationcalculations/{Year}", logPrefix, year);
         logger.LogInformation("{Logprefix}: PrnController - GetObligationCalculation: request to get Obligation Calculation for organisations {Organisation} for {Year}", logPrefix, string.Join(", ", organisationIds), year);
 
-        if (organisationIds.Count == 0)
-        {
-            logger.LogError("{Logprefix}: PrnController - GetObligationCalculation: Organisation Ids list can't be empty. {Organisations}", logPrefix, organisationIds);
-            return Ok(new ObligationModel { ObligationData = [], NumberOfPrnsAwaitingAcceptance = 0 });
-        }
+        //if (organisationIds.Count == 0)
+        //{
+        //    logger.LogError("{Logprefix}: PrnController - GetObligationCalculation: Organisation Ids list can't be empty. {Organisations}", logPrefix, organisationIds);
+        //    return Ok(new ObligationModel { ObligationData = [], NumberOfPrnsAwaitingAcceptance = 0 });
+        //}
 
         if (year < _config.StartYear || year > _config.EndYear)
         {
