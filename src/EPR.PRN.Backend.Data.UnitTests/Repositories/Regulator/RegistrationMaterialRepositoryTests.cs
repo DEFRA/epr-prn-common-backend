eee2484238b4f48a3d27f5558b6186908410333d
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-04-16 07:25:08 +0100

    feature/523465 update registration outcome

--- src/EPR.PRN.Backend.Data.UnitTests/Repositories/Regulator/RegistrationMaterialRepositoryTests.cs
+++ src/EPR.PRN.Backend.Data.UnitTests/Repositories/Regulator/RegistrationMaterialRepositoryTests.cs
@@ -2,6 +2,10 @@
 using EPR.PRN.Backend.Data.Interfaces.Regulator;
 using EPR.PRN.Backend.Data.Repositories.Regulator;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EPR.PRN.Backend.Data.UnitTests.Repositories.Regulator;
 
@@ -26,75 +30,6 @@ public class RegistrationMaterialRepositoryTests
 
     private void SeedDatabase()
     {
-        var registration = new Registration
-        {
-            Id = 1,
-            ApplicationTypeId = 1,
-            ExternalId = "TestExternalId"
-        };
-        _context.Registrations.Add(registration);
-
-        var material = new RegistrationMaterial
-        {
-            Id = 1,
-            RegistrationId = 1,
-            MaterialId = 1,
-            StatusID = 1,
-            ReferenceNumber = "REF12345",
-            Comments = "Initial comment"
-        };
-        _context.RegistrationMaterials.Add(material);
-
-        var lookupMaterial = new LookupMaterial
-        {
-            Id = 1,
-            MaterialCode = "PLSTC",
-            MaterialName = "Plastic"
-        };
-        _context.LookupMaterials.Add(lookupMaterial);
-
-        var status = new LookupRegistrationMaterialStatus
-        {
-            Id = 1,
-            Name = "Completed"
-        };
-        _context.LookupRegistrationMaterialStatuses.Add(status);
-
-        var lookupTask = new LookupRegulatorTask
-        {
-            Id = 1,
-            Name = "SiteAddressAndContactDetails",
-            IsMaterialSpecific = false,
-            ApplicationTypeId = 1,
-            JourneyTypeId = 1
-        };
-        _context.LookupTasks.Add(lookupTask);
-
-        var taskStatus = new LookupTaskStatus
-        {
-            Id = 1,
-            Name = "Not Started"
-        };
-        _context.LookupTaskStatuses.Add(taskStatus);
-
-        var regTaskStatus = new RegulatorRegistrationTaskStatus
-        {
-            Id = 1,
-            RegistrationId = 1,
-            TaskId = 1,
-            TaskStatusId = 1
-        };
-        _context.RegulatorRegistrationTaskStatus.Add(regTaskStatus);
-
-        var appTaskStatus = new RegulatorApplicationTaskStatus
-        {
-            Id = 1,
-            RegistrationMaterialId = 1,
-            TaskId = 1,
-            TaskStatusId = 1
-        };
-        _context.RegulatorApplicationTaskStatus.Add(appTaskStatus);
-
         var address = new LookupAddress
         {
             Id = 1,
@@ -105,10 +40,72 @@ public class RegistrationMaterialRepositoryTests
             PostCode = "12345",
             TownCity = "Munich"
         };
+
+        var taskStatus = new LookupTaskStatus { Id = 1, Name = "Not Started" };
+        var task = new LookupRegulatorTask
+        {
+            Id = 1,
+            Name = "SiteAddressAndContactDetails",
+            IsMaterialSpecific = false,
+            ApplicationTypeId = 1,
+            JourneyTypeId = 1
+        };
+        var materialStatus = new LookupRegistrationMaterialStatus { Id = 1, Name = "Completed" };
+        var lookupMaterial = new LookupMaterial { Id = 1, MaterialCode = "PLSTC", MaterialName = "Plastic" };
+
+        var material = new RegistrationMaterial
+        {
+            Id = 1,
+            MaterialId = 1,
+            StatusID = 1,
+            ReferenceNumber = "REF12345",
+            Comments = "Initial comment",
+            Status = materialStatus,
+            Material = lookupMaterial,
+            Tasks = new List<RegulatorApplicationTaskStatus>
+        {
+            new RegulatorApplicationTaskStatus
+            {
+                Id = 1,
+                TaskId = 1,
+                TaskStatusId = 1,
+                TaskStatus = taskStatus,
+                Task = task
+            }
+        }
+        };
+
+        var registration = new Registration
+        {
+            Id = 1,
+            ApplicationTypeId = 1,
+            ExternalId = "TestExternalId",
+            ReprocessingSiteAddress = address,
+            BusinessAddress = address,
+            Tasks = new List<RegulatorRegistrationTaskStatus>
+        {
+            new RegulatorRegistrationTaskStatus
+            {
+                Id = 1,
+                TaskId = 1,
+                TaskStatusId = 1,
+                TaskStatus = taskStatus,
+                Task = task
+            }
+        },
+            Materials = new List<RegistrationMaterial> { material }
+        };
+
+        _context.LookupTasks.Add(task);
+        _context.LookupTaskStatuses.Add(taskStatus);
+        _context.LookupRegistrationMaterialStatuses.Add(materialStatus);
+        _context.LookupMaterials.Add(lookupMaterial);
         _context.LookupAddresses.Add(address);
+        _context.Registrations.Add(registration);
 
         _context.SaveChanges();
     }
+
 
     [TestMethod]
     public async Task GetRegistrationById_ShouldReturnRegistration_WhenRegistrationExists()
@@ -116,6 +113,8 @@ public class RegistrationMaterialRepositoryTests
         var result = await _repository.GetRegistrationById(1);
         Assert.IsNotNull(result);
         Assert.AreEqual(1, result.Id);
+        Assert.IsNotNull(result.Materials);
+        Assert.IsNotNull(result.Tasks);
     }
 
     [TestMethod]
@@ -136,8 +135,8 @@ public class RegistrationMaterialRepositoryTests
     public async Task UpdateRegistrationOutCome_ShouldUpdateFieldsCorrectly()
     {
         var newStatusId = 2;
-        var comment = "Updated";
-        var newReference = "REF99999";
+        var comment = "Updated comment";
+        var newReference = "REFUPDATED";
 
         await _repository.UpdateRegistrationOutCome(1, newStatusId, comment, newReference);
         var updated = await _context.RegistrationMaterials.FindAsync(1);
@@ -145,6 +144,32 @@ public class RegistrationMaterialRepositoryTests
         Assert.AreEqual(newStatusId, updated.StatusID);
         Assert.AreEqual(comment, updated.Comments);
         Assert.AreEqual(newReference, updated.ReferenceNumber);
+        Assert.IsNotNull(updated.StatusUpdatedDate);
+        Assert.AreEqual("Test User", updated.StatusUpdatedBy);
+    }
+
+    [TestMethod]
+    public async Task UpdateRegistrationOutCome_ShouldThrow_WhenMaterialNotFound()
+    {
+        await Assert.ThrowsExceptionAsync<KeyNotFoundException>(() =>
+            _repository.UpdateRegistrationOutCome(999, 1, "Test", "REF"));
+    }
+
+    [TestMethod]
+    public async Task GetRegistrationMaterialById_ShouldReturnMaterial_WhenExists()
+    {
+        var material = await _repository.GetRegistrationMaterialById(1);
+        Assert.IsNotNull(material);
+        Assert.AreEqual("REF12345", material.ReferenceNumber);
+        Assert.IsNotNull(material.Material);
+        Assert.IsNotNull(material.Status);
+        Assert.IsNotNull(material.Registration);
+    }
+
+    [TestMethod]
+    public async Task GetRegistrationMaterialById_ShouldThrow_WhenNotFound()
+    {
+        await Assert.ThrowsExceptionAsync<KeyNotFoundException>(() => _repository.GetRegistrationMaterialById(999));
     }
 
     [TestCleanup]
