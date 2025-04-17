29697e5141c4cf40b3d051af6895dbc86aa85331
mw-forster <mark.forster@eviden.com>
2025-04-17 10:12:29 +0100

    Made UserName Nullable

--- src/EPR.PRN.Backend.API/Commands/UpdateRegulatorTaskCommandBase.cs
+++ src/EPR.PRN.Backend.API/Commands/UpdateRegulatorTaskCommandBase.cs
@@ -11,5 +11,5 @@ public abstract class UpdateRegulatorTaskCommandBase : IRequest
     public required RegulatorTaskStatus Status { get; set; }
     [MaxLength(500)]
     public string? Comments { get; set; } = string.Empty;
-    public  string UserName { get; set; }
+    public string? UserName { get; set; } = string.Empty;
 }
