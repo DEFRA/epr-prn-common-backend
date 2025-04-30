5fde84e8859e7191f8c255fdd2dc795d5dba025f
mw-forster <mark.forster@eviden.com>
2025-04-30 12:05:31 +0100

    Fixed Lookup Spelling

--- src/EPR.PRN.Backend.API.Common/Constants/PermitTypes.cs
+++ src/EPR.PRN.Backend.API.Common/Constants/PermitTypes.cs
@@ -6,7 +6,7 @@ namespace EPR.PRN.Backend.API.Common.Constants;
 public static class PermitTypes
 {
     public const string WasteExemption = "Waste Exemption";
-    public const string PollutionPreventionAndControlPermit = "Pollution,Prevention and Contril(PPC) permit";
+    public const string PollutionPreventionAndControlPermit = "Pollution,Prevention and Control(PPC) permit";
     public const string WasteManagementLicence = "Waste Management License";
     public const string InstallationPermit = "Installation Permit";
     public const string EnvironmentalPermitOrWasteManagementLicence = "Environmental permit or waste management license";
