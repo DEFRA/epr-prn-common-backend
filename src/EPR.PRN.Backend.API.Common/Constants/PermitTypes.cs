1feaf39b042b097e57c531d728baaf886ebd824c
mw-forster <mark.forster@eviden.com>
2025-05-02 15:19:43 +0100

    Updated Permit Type Text

--- src/EPR.PRN.Backend.API.Common/Constants/PermitTypes.cs
+++ src/EPR.PRN.Backend.API.Common/Constants/PermitTypes.cs
@@ -7,7 +7,7 @@ public static class PermitTypes
 {
     public const string WasteExemption = "Waste Exemption";
     public const string PollutionPreventionAndControlPermit = "Pollution,Prevention and Control(PPC) permit";
-    public const string WasteManagementLicence = "Waste Management License";
+    public const string WasteManagementLicence = "Waste Management Licence";
     public const string InstallationPermit = "Installation Permit";
-    public const string EnvironmentalPermitOrWasteManagementLicence = "Environmental permit or waste management license";
+    public const string EnvironmentalPermitOrWasteManagementLicence = "Environmental permit or waste management licence";
 }
