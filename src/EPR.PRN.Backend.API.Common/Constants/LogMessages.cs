588d52656835cc58ec867f74add63919d5362966
Manish Agarwal <manish.agarwal.external@eviden.com>
2025-05-19 08:46:37 +0100

    review comment fixed

--- src/EPR.PRN.Backend.API.Common/Constants/LogMessages.cs
+++ src/EPR.PRN.Backend.API.Common/Constants/LogMessages.cs
@@ -6,7 +6,7 @@ public static class LogMessages
     public const string SummaryInfoMaterial = "Attempting to get summary info for a material";
     public const string OutcomeMaterialRegistration = "UpdateRegistrationOutcome called with Id: {Id}";
     public const string RegistrationSiteAddress = "Attempting to get site address info for registration id :{Id}";
-    public const string MaterialAuthorization = "Attempting to get  material authorization  site of registration id :{Id}";
+    public const string MaterialAuthorization = "Attempting to get material authorisation  site of registration id :{Id}";
     public const string RegistrationMaterialpaymentFees = "Attempting to get registration material payment fee by id :{Id}";
     public const string UpdateRegulatorApplicationTask = "UpdateRegulatorApplicationTask";
     public const string UpdateRegulatorRegistrationTask = "UpdateRegulatorRegistrationTask";
