dd5d86b7fb0986f2585abc6ec07a2634b11c85ac
Robert Smith <199087159+RobertSmithEviden@users.noreply.github.com>
2025-05-21 09:00:32 +0100

    542728: PrnTonnage - Unit tests for backend components. (#227)

---
+++ src/EPR.PRN.Backend.API.UnitTests/Controllers/AccreditationControllerTests.cs
@@ -0,0 +1,101 @@
+﻿using EPR.PRN.Backend.API.Controllers;
+using EPR.PRN.Backend.API.Dto.Accreditation;
+using EPR.PRN.Backend.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using FluentAssertions;
+
+namespace EPR.PRN.Backend.API.UnitTests.Controllers;
+
+[TestClass]
+public class AccreditationControllerTests
+{
+    private Mock<IAccreditationService> _serviceMock;
+    private AccreditationController _controller;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _serviceMock = new Mock<IAccreditationService>();
+        _controller = new AccreditationController(_serviceMock.Object);
+    }
+
+    [TestMethod]
+    public async Task Get_ShouldReturnOk_WhenAccreditationExists()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var accreditation = new AccreditationDto { ExternalId = accreditationId };
+        _serviceMock.Setup(s => s.GetAccreditationById(accreditationId))
+            .ReturnsAsync(accreditation);
+
+        // Act
+        var result = await _controller.Get(accreditationId);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().Be(accreditation);
+        _serviceMock.Verify(s => s.GetAccreditationById(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task Get_ShouldReturnNotFound_WhenAccreditationDoesNotExist()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        _serviceMock.Setup(s => s.GetAccreditationById(accreditationId))
+            .ReturnsAsync((AccreditationDto)null);
+
+        // Act
+        var result = await _controller.Get(accreditationId);
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
+        _serviceMock.Verify(s => s.GetAccreditationById(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task PostWithNewAccreditation_ShouldReturnOk_WithCreatedAccreditation()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var request = new AccreditationRequestDto { OrganisationId = Guid.NewGuid(), AccreditationYear = 2026 };
+        _serviceMock.Setup(s => s.CreateAccreditation(request))
+            .ReturnsAsync(accreditationId);
+
+        var accreditation = new AccreditationDto { ExternalId = accreditationId, AccreditationYear = 2026 };
+        _serviceMock.Setup(s => s.GetAccreditationById(accreditationId))
+            .ReturnsAsync(accreditation);
+
+        // Act
+        var result = await _controller.Post(request);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().Be(accreditation);
+        _serviceMock.Verify(s => s.CreateAccreditation(request), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task PostWithExistingAccreditation_ShouldReturnOk_WithUpdatedAccreditation()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var request = new AccreditationRequestDto { ExternalId = accreditationId, OrganisationId = Guid.NewGuid(), AccreditationYear = 2026 };
+
+        var accreditation = new AccreditationDto { ExternalId = accreditationId, OrganisationId = Guid.NewGuid(), AccreditationYear = 2026 };
+        _serviceMock.Setup(s => s.GetAccreditationById(accreditationId))
+            .ReturnsAsync(accreditation);
+
+        // Act
+        var result = await _controller.Post(request);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().Be(accreditation);
+        _serviceMock.Verify(s => s.UpdateAccreditation(request), Times.Once);
+    }
+}
---
+++ src/EPR.PRN.Backend.API.UnitTests/Services/Accreditations/AccreditationPrnIssueAuthServiceTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using EPR.PRN.Backend.API.Dto.Accreditation;
+using EPR.PRN.Backend.API.Profiles;
+using EPR.PRN.Backend.API.Services;
+using EPR.PRN.Backend.Data.DataModels.Accreditations;
+using EPR.PRN.Backend.Data.Interfaces.Accreditations;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace EPR.PRN.Backend.API.UnitTests.Services.Accreditations;
+
+[TestClass]
+public class AccreditationPrnIssueAuthServiceTests
+{
+    private Mock<IAccreditationPrnIssueAuthRepository> _repositoryMock;
+    private Mock<ILogger<AccreditationPrnIssueAuthService>> _loggerMock;
+    private Mock<IConfiguration> _configurationMock;
+    private AccreditationPrnIssueAuthService _service;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repositoryMock = new Mock<IAccreditationPrnIssueAuthRepository>();
+        _loggerMock = new Mock<ILogger<AccreditationPrnIssueAuthService>>();
+        _configurationMock = new Mock<IConfiguration>();
+        _configurationMock.Setup(c => c["LogPrefix"]).Returns("[EPR.PRN.Backend]");
+
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<AccreditationProfile>();
+        });
+        var mapper = config.CreateMapper();
+
+        _service = new AccreditationPrnIssueAuthService(
+            _repositoryMock.Object,
+            mapper,
+            _loggerMock.Object,
+            _configurationMock.Object
+        );
+    }
+
+    [TestMethod]
+    public async Task GetByAccreditationId_ReturnsMappedDtos_WhenEntitiesExist()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var entities = new List<AccreditationPrnIssueAuth>
+        {
+            new AccreditationPrnIssueAuth { ExternalId = Guid.NewGuid(), AccreditationExternalId = accreditationId, PersonExternalId = Guid.NewGuid() },
+            new AccreditationPrnIssueAuth { ExternalId = Guid.NewGuid(), AccreditationExternalId = accreditationId, PersonExternalId = Guid.NewGuid() }
+        };
+        var dtos = new List<AccreditationPrnIssueAuthDto>
+        {
+            new AccreditationPrnIssueAuthDto { ExternalId = entities[0].ExternalId, AccreditationExternalId = accreditationId, PersonExternalId = entities[0].PersonExternalId },
+            new AccreditationPrnIssueAuthDto { ExternalId = entities[1].ExternalId, AccreditationExternalId = accreditationId, PersonExternalId = entities[1].PersonExternalId }
+        };
+
+        _repositoryMock.Setup(r => r.GetByAccreditationId(accreditationId)).ReturnsAsync(entities);
+
+        // Act
+        var result = await _service.GetByAccreditationId(accreditationId);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(dtos);
+        _repositoryMock.Verify(r => r.GetByAccreditationId(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task GetByAccreditationId_ReturnsEmptyList_WhenNoEntities()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var entities = new List<AccreditationPrnIssueAuth>();
+
+        _repositoryMock.Setup(r => r.GetByAccreditationId(accreditationId)).ReturnsAsync(entities);
+
+        // Act
+        var result = await _service.GetByAccreditationId(accreditationId);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+        _repositoryMock.Verify(r => r.GetByAccreditationId(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task ReplaceAllByAccreditationId_CallsRepositoryWithCorrectParameters()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var request = new List<AccreditationPrnIssueAuthRequestDto>
+        {
+            new AccreditationPrnIssueAuthRequestDto { PersonExternalId = Guid.NewGuid() },
+            new AccreditationPrnIssueAuthRequestDto { PersonExternalId = Guid.NewGuid() }
+        };
+        
+        // Act
+        await _service.ReplaceAllByAccreditationId(accreditationId, request);
+
+        // Assert
+        _repositoryMock.Verify(r => r.ReplaceAllByAccreditationId(accreditationId, It.Is<List<AccreditationPrnIssueAuth>>(x =>
+            x[0].PersonExternalId == request[0].PersonExternalId &&
+            x[1].PersonExternalId == request[1].PersonExternalId)), Times.Once);
+    }
+}
+
