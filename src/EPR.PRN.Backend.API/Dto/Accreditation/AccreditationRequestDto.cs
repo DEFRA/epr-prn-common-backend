f26ab1723fa4824c11f9402df3a745a1ad5ceecb
Yasin Erdogan <yasin.erdogan.external@eviden.com>
2025-05-27 15:06:12 +0100

    557935 - OtherPercentage and OtherNotes added

--- src/EPR.PRN.Backend.API/Dto/Accreditation/AccreditationRequestDto.cs
+++ src/EPR.PRN.Backend.API/Dto/Accreditation/AccreditationRequestDto.cs
@@ -21,11 +21,13 @@ namespace EPR.PRN.Backend.API.Dto.Accreditation
         public decimal? NewUsesPercentage { get; set; }
         public decimal? NewMarketsPercentage { get; set; }
         public decimal? CommunicationsPercentage { get; set; }
+        public decimal? OtherPercentage { get; set; }
         public string? InfrastructureNotes { get; set; }
         public string? PackagingWasteNotes { get; set; }
         public string? BusinessCollectionsNotes { get; set; }
         public string? NewUsesNotes { get; set; }
         public string? NewMarketsNotes { get; set; }
         public string? CommunicationsNotes { get; set; }
+        public string? OtherNotes { get; set; }
     }
 }
