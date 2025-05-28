f2b5a0c2967179edb7e96ed0698b3b8c421de0ee
Adrian Clare <adrian.clare.external@eviden.com>
2025-05-28 15:41:37 +0100

    add fix

--- src/EPR.PRN.Backend.Data/EprContext.cs
+++ src/EPR.PRN.Backend.Data/EprContext.cs
@@ -298,6 +298,7 @@ namespace EPR.PRN.Backend.Data
         public virtual DbSet<LookupTaskStatus> LookupTaskStatuses { get; set; }
         public virtual DbSet<Address> LookupAddresses { get; set; }
         public virtual DbSet<LookupPeriod> LookupPeriod { get; set; }
+        public virtual DbSet<LookupMaterialPermit> LookupMaterialPermit { get; set; }
         public virtual DbSet<Accreditation> Accreditations { get; set; }
     }
 }
