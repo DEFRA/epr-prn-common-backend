2a5089ea07fd0e27c38bd2b7892a6e9ccfacf94c
Srinivas Dudam (ext) <srinivas.dudam.external@eviden.com>
2025-06-10 08:09:14 +0100

    code tidy up #263

--- src/EPR.PRN.Backend.API/Commands/CreateMaterialExemptionReferenceCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using EPR.PRN.Backend.Obligation.Models;
-using MediatR;
-
-namespace EPR.PRN.Backend.API.Commands;
-
-public class CreateMaterialExemptionReferenceCommand :IRequest<bool>
-{
-    public required List<MaterialExemptionReferenceRequest> MaterialExemptionReferences { get; set; }
-}
