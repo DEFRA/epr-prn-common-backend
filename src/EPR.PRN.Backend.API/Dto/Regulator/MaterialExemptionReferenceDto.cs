3e1aaf2cfed85b3e6805c8cd6a79f9b26c43eba5
SrinivasDudam-Eviden <srinivas.dudam.external@eviden.com>
2025-06-13 09:36:00 +0100

    Feature/543123 save exemption references be (#263)
    * adding exemption reference to database #543123
    
    * Unit Test for Save-exemption-references #543123
    
    * added unit tests #543123
    
    * create an end point for creating registration material and material exemption references
    
    * saving exemption references to database #543123
    
    * added unitests #543123
    
    * sonar issues fix #543123
    
    Related Work Items: #5
    
    * added unit tests #543123
    
    * sonar fixes #263
    
    * code tidy up remove unsed code #543123
    
    * code tidy up #263
    
    * added unit tests #263
    
    * PR comments fix #263

---
+++ src/EPR.PRN.Backend.API/Dto/Regulator/MaterialExemptionReferenceDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.PRN.Backend.API.Dto.Regulator;
+
+[ExcludeFromCodeCoverage]
+public class MaterialExemptionReferenceDto
+{
+    public required string ReferenceNumber { get; set; }
+}
