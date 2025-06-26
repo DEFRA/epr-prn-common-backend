82ded74b455ed4f650088fce89ed3b1e93cc15f7
John Fitzgerald <john.fitzgerald.external@eviden.com>
2025-06-26 15:44:18 +0100

    fixes after merge

--- src/EPR.PRN.Backend.API.UnitTests/Validators/UpdateRegistrationTaskStatusCommandValidatorTests.cs
+++ src/EPR.PRN.Backend.API.UnitTests/Validators/UpdateRegistrationTaskStatusCommandValidatorTests.cs
@@ -35,28 +35,5 @@ public class UpdateRegistrationTaskStatusCommandValidatorTests
 
         // Assert
         result.IsValid.Should().BeTrue();
-    }
-
-    [TestMethod]
-    [DataRow(TaskStatuses.CannotStartYet)]
-    [DataRow(TaskStatuses.NotStarted)]
-    public async Task Validate_ShouldFail_ForDisallowedStatuses(TaskStatuses invalidStatus)
-    {
-        // Arrange
-        var registrationId = Guid.NewGuid();
-        var command = new UpdateRegistrationTaskStatusCommand 
-        { 
-            Status = invalidStatus,
-            RegistrationId = registrationId,
-            TaskName = "Test Task"
-        };
-    
-        // Act
-        var result = await _validator.TestValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(x => x.Status)
-              .WithErrorMessage("Invalid Status value");
-    }
+    } 
 }
