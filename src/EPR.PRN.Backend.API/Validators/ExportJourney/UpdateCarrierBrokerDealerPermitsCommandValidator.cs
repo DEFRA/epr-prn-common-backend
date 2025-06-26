e0060681f4b86b80f857bacdc9003ad931809a64
spar-eviden <simon.parsons.external@eviden.com>
2025-06-26 14:12:39 +0000

    Update validator and add unit test coverage

--- src/EPR.PRN.Backend.API/Validators/ExportJourney/UpdateCarrierBrokerDealerPermitsCommandValidator.cs
+++ src/EPR.PRN.Backend.API/Validators/ExportJourney/UpdateCarrierBrokerDealerPermitsCommandValidator.cs
@@ -11,20 +11,30 @@ namespace EPR.PRN.Backend.API.Validators.ExportJourney
             var allowedStatuses = new[] { RegulatorTaskStatus.Queried, RegulatorTaskStatus.Completed };
 
             RuleFor(x => x.Dto.WasteCarrierBrokerDealerRegistration)
-                .MaximumLength(16).WithMessage("WasteCarrierBrokerDealerRegistration must not exceed 16 characters");
+                .MaximumLength(16)
+                .WithMessage("WasteCarrierBrokerDealerRegistration must not exceed 16 characters");
 
             RuleFor(x => x.Dto.WasteLicenseOrPermitNumber)
-                .MaximumLength(20).WithMessage("WasteLicenseOrPermitNumber must not exceed 20 characters");
+                .MaximumLength(20)
+                .WithMessage("WasteLicenseOrPermitNumber must not exceed 20 characters");
 
             RuleFor(x => x.Dto.PpcNumber)
-                .MaximumLength(20).WithMessage("PpcNumber must not exceed 20 characters");
+                .MaximumLength(20)
+                .WithMessage("PpcNumber must not exceed 20 characters");
 
             RuleFor(x => x.Dto.WasteExemptionReference)
                 .NotNull()
-                .Must(item => item.Count <= 5).WithMessage("WasteExemptionReference must not exceed 5 values")
-                .ForEach(item =>
-                    item.MaximumLength(20).WithMessage("WasteExemptionReference must not exceed 20 characters")
-                );
+                .WithMessage("WasteExemptionReference is required");
+
+            RuleFor(x => x.Dto.WasteExemptionReference)
+                .Must(list => list == null || list.Count <= 5)
+                .WithMessage("WasteExemptionReference must not exceed 5 values")
+                .When(x => x.Dto.WasteExemptionReference != null);
+
+            RuleForEach(x => x.Dto.WasteExemptionReference)
+                .MaximumLength(20)
+                .WithMessage("WasteExemptionReference must not exceed 20 characters")
+                .When(x => x.Dto.WasteExemptionReference != null);
         }
     }
 }
