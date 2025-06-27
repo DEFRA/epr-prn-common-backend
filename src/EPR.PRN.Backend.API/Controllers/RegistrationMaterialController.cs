2d0972e2b05f8e3744f9d5a7ee82ed35a6241a37
Paul Cripps (ext) <paul.cripps.external@eviden.com>
2025-06-27 14:14:59 +0100

    Merge down from main

--- src/EPR.PRN.Backend.API/Controllers/RegistrationMaterialController.cs
+++ src/EPR.PRN.Backend.API/Controllers/RegistrationMaterialController.cs
@@ -168,45 +168,4 @@ public class RegistrationMaterialController(
 
         return Ok();
     }
-
-    [HttpPost("registrationMaterials/{id:Guid}/permits")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OkResult))]
-    [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
-    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
-    [SwaggerOperation(
-        Summary = "updates an existing registration material",
-        Description = "attempting to update the registration material."
-    )]
-    [SwaggerResponse(StatusCodes.Status204NoContent, "Returns No Content")]
-    [SwaggerResponse(StatusCodes.Status400BadRequest, "If the request is invalid or a validation error occurs.", typeof(ProblemDetails))]
-    [SwaggerResponse(StatusCodes.Status404NotFound, "If an existing registration is not found", typeof(ProblemDetails))]
-    [SwaggerResponse(StatusCodes.Status500InternalServerError, "If an unexpected error occurs.", typeof(ContentResult))]
-    public async Task<IActionResult> UpdateRegistrationMaterialPermits([FromRoute] Guid id, [FromBody] UpdateRegistrationMaterialPermitsCommand command)
-    {
-
-        logger.LogInformation(LogMessages.UpdateRegistrationMaterial, id);
-
-        command.RegistrationMaterialId = id;
-
-        await mediator.Send(command);
-
-        return NoContent();
-    }
-
-    [HttpGet("registrationMaterials/permitTypes")]
-    [ProducesResponseType(typeof(List<MaterialsPermitTypeDto>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ContentResult), StatusCodes.Status500InternalServerError)]
-    [SwaggerOperation(
-        Summary = "Retrieves list of material permit types",
-        Description = "Returns a list of material permit types used during registration."
-    )]
-    [SwaggerResponse(StatusCodes.Status200OK, "Successfully retrieved permit types.", typeof(List<MaterialsPermitTypeDto>))]
-    [SwaggerResponse(StatusCodes.Status500InternalServerError, "An unexpected error occurred.", typeof(ContentResult))]
-    public async Task<IActionResult> GetMaterialsPermitTypes()
-    {
-        logger.LogInformation(LogMessages.GetMaterialsPermitTypes);
-
-        var result = await mediator.Send(new GetMaterialsPermitTypesQuery());
-        return Ok(result);
-    }
 }
