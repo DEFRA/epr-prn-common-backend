29e47337bce93fd0f646baa786ed361c379f721f
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-06-29 20:09:39 +0100

    fixed validastion

--- src/EPR.PRN.Backend.API.UnitTests/Validators/UpdateRegistrationMaterialPermitsCommandValidatorTests.cs
+++ src/EPR.PRN.Backend.API.UnitTests/Validators/UpdateRegistrationMaterialPermitsCommandValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoFixture;
 using EPR.PRN.Backend.API.Commands;
+using EPR.PRN.Backend.API.Common.Enums;
 using EPR.PRN.Backend.API.Validators;
 using FluentAssertions;
 using FluentValidation.TestHelper;
@@ -35,24 +36,39 @@ public class UpdateRegistrationMaterialPermitsCommandValidatorTests
     }
 
     [TestMethod]
-    public void Should_Have_Error_When_PermitNumber_Is_Null_Or_Empty()
+    [DataRow(MaterialPermitType.WasteExemption)]
+    [DataRow(MaterialPermitType.PollutionPreventionAndControlPermit)]
+    [DataRow(MaterialPermitType.WasteManagementLicence)]
+    [DataRow(MaterialPermitType.InstallationPermit)]
+    [DataRow(MaterialPermitType.EnvironmentalPermitOrWasteManagementLicence)]
+    public void Should_Have_Error_When_PermitNumber_Is_Null_Or_Empty_AndNotWasteExemptions(int permitId)
     {
         var commandWithNull = _fixture.Build<UpdateRegistrationMaterialPermitsCommand>()
+            .With(x => x.PermitTypeId, permitId)
             .With(x => x.PermitNumber, (string)null)
             .Create();
 
         var commandWithEmpty = _fixture.Build<UpdateRegistrationMaterialPermitsCommand>()
+            .With(x => x.PermitTypeId, permitId)
             .With(x => x.PermitNumber, string.Empty)
             .Create();
 
         var resultNull = _validator.TestValidate(commandWithNull);
         var resultEmpty = _validator.TestValidate(commandWithEmpty);
 
-        resultNull.ShouldHaveValidationErrorFor(x => x.PermitNumber)
-            .WithErrorMessage("PermitNumber is required");
+        if (permitId is not (int)MaterialPermitType.WasteExemption)
+        {
+            resultNull.ShouldHaveValidationErrorFor(x => x.PermitNumber)
+                .WithErrorMessage("PermitNumber is required");
 
-        resultEmpty.ShouldHaveValidationErrorFor(x => x.PermitNumber)
-            .WithErrorMessage("PermitNumber is required");
+            resultEmpty.ShouldHaveValidationErrorFor(x => x.PermitNumber)
+                .WithErrorMessage("PermitNumber is required");
+        }
+        else
+        {
+            resultNull.ShouldNotHaveValidationErrorFor(x => x.PermitNumber);
+            resultEmpty.ShouldNotHaveValidationErrorFor(x => x.PermitNumber);
+        }
     }
 
     [TestMethod]
