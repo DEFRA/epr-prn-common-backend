81ff251467f96b665930bf23d9dca20f72a917d6
Mark Forster <160658973+MW-Forster@users.noreply.github.com>
2025-06-30 13:01:05 +0100

    Feature/563047 ex registration   overseas reprocessor details (#299)
    * Get Countries Lookup
    
    * Added All World Countries
    
    * Updated Countries
    
    * Fix Build
    
    * Updates for SonarQube
    
    * Added Migration

---
+++ src/EPR.PRN.Backend.Data/Repositories/Lookup/CountryRepository.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EPR.PRN.Backend.Data.DataModels.Registrations;
+using EPR.PRN.Backend.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPR.PRN.Backend.Data.Repositories.Lookup;
+
+public class CountryRepository : ILookupCountryRepository
+{
+    private readonly EprContext _context;
+
+    public CountryRepository(EprContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<LookupCountry>> GetAllAsync()
+    {
+        return await _context.LookupCountries
+            .OrderBy(c => c.Name)
+            .ToListAsync();
+    }
+}
