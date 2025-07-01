52c2658c70e38e70299517501c29b3087de5dbab
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-07-01 09:40:49 +0100

    temp

--- src/EPR.PRN.Backend.API.UnitTests/Handlers/GetRegistrationTaskOverviewDetailByIdHandlerTests.cs
+++ src/EPR.PRN.Backend.API.UnitTests/Handlers/GetRegistrationTaskOverviewDetailByIdHandlerTests.cs
@@ -1,8 +1,6 @@
-﻿using AutoMapper;
-using EPR.PRN.Backend.API.Dto.Regulator;
+﻿using EPR.PRN.Backend.API.Dto.Regulator;
 using EPR.PRN.Backend.API.Handlers;
 using EPR.PRN.Backend.API.Handlers.Regulator;
-using EPR.PRN.Backend.API.Profiles.Regulator;
 using EPR.PRN.Backend.Data.DataModels.Registrations;
 using EPR.PRN.Backend.Data.Interfaces;
 using FluentAssertions;
@@ -11,22 +9,16 @@ using Moq;
 namespace EPR.PRN.Backend.API.UnitTests.Handlers;
 
 [TestClass]
-public class GetRegistrationTaskOverviewDetailByIdHandlerTests
+public class GetRegistrationTaskOverviewDetailByIdHandlerTests : HandlerTestsBase
 {
     private Mock<IRegistrationRepository> _mockRegistrationRepository;
     private GetRegistrationTaskOverviewDetailByIdHandler _handler;
-    private IMapper _mapper;
 
     [TestInitialize]
     public void TestInitialize()
     {
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<RegistrationTaskOverviewProfile>();
-        });
-        _mapper = config.CreateMapper();
         _mockRegistrationRepository = new Mock<IRegistrationRepository>();
-        _handler = new GetRegistrationTaskOverviewDetailByIdHandler(_mockRegistrationRepository.Object, _mapper);
+        _handler = new GetRegistrationTaskOverviewDetailByIdHandler(_mockRegistrationRepository.Object, Mapper.CreateMapper());
     }
 
     [TestMethod]
@@ -55,7 +47,8 @@ public class GetRegistrationTaskOverviewDetailByIdHandlerTests
                         JourneyTypeId = 1
                     }
                 }
-            ]
+            ],
+            Materials = []
         };
         
         // Expectations
@@ -83,21 +76,22 @@ public class GetRegistrationTaskOverviewDetailByIdHandlerTests
             Regulator = "EA",
             OrganisationId = organisationId,
             Id = registrationId,
-            Materials = new List<RegistrationMaterialDto>(),
-            Tasks = new List<RegistrationTaskDto>
-            {
+            Materials = [],
+            Tasks =
+            [
                 new()
                 {
                     Id = Guid.Empty,
                     TaskName = "task",
                     Status = null
                 },
+
                 new()
                 {
                     TaskName = "task 1",
                     Status = "NotStarted"
                 }
-            }
+            ]
         });
     }
 
@@ -138,6 +132,23 @@ public class GetRegistrationTaskOverviewDetailByIdHandlerTests
                         Id = 1,
                         MaterialCode = "STL",
                         MaterialName = "steel"
+                    },
+                    IsMaterialRegistered = true,
+                    Tasks = new List<RegulatorApplicationTaskStatus>
+                    {
+                        new ()
+                        {
+                            Id = 1, 
+                            RegistrationMaterialId = 1,
+                            Task = new LookupRegulatorTask
+                            {
+                                Id = 1,
+                                Name = "task",
+                                ApplicationTypeId = 1,
+                                IsMaterialSpecific = true,
+                                JourneyTypeId = 1
+                            }
+                        }
                     }
                 }
             ]
@@ -168,21 +179,22 @@ public class GetRegistrationTaskOverviewDetailByIdHandlerTests
             Regulator = "EA",
             OrganisationId = organisationId,
             Id = registrationId,
-            Materials = new List<RegistrationMaterialDto>(),
-            Tasks = new List<RegistrationTaskDto>
-            {
+            Materials = [],
+            Tasks =
+            [
                 new()
                 {
                     Id = Guid.Empty,
                     TaskName = "task",
                     Status = null
                 },
+
                 new()
                 {
                     TaskName = "task 1",
                     Status = "NotStarted"
                 }
-            }
+            ]
         });
     }
 }
--- src/EPR.PRN.Backend.API.UnitTests/Mapper/Registration/RegistrationTaskOverviewProfileTests.cs
+++ src/EPR.PRN.Backend.API.UnitTests/Mapper/Registration/RegistrationTaskOverviewProfileTests.cs
@@ -1,29 +1,52 @@
 ﻿using EPR.PRN.Backend.API.Dto.Regulator;
-using EPR.PRN.Backend.API.Profiles.Regulator;
+using EPR.PRN.Backend.Data.DataModels.Registrations;
 using FluentAssertions;
 
 namespace EPR.PRN.Backend.API.UnitTests.Mapper.Registration;
 
 [TestClass]
-public class RegistrationTaskOverviewProfileTests : MappingTestsBase<RegistrationTaskOverviewProfile>
+public class RegistrationTaskOverviewProfileTests : MappingProfileTestBase
 {
     [TestMethod]
     public void Registration_To_RegistrationTaskOverviewDto_EnsureCorrectMapping()
     {
         // Arrange
         var externalId = Guid.NewGuid();
-        var mapper = CreateMapper();
+        var mapper = Mapper.CreateMapper();
         var source = new Data.DataModels.Registrations.Registration
         {
             Id = 1,
             ExternalId = externalId,
-            ApplicationTypeId = 1
+            ApplicationTypeId = 1,
+            Materials = new List<RegistrationMaterial>
+            {
+                new ()
+                {
+                    Id = 1,
+                    Material = new LookupMaterial
+                    {
+                        Id = 1, MaterialCode = "STL", MaterialName = "Steel"
+                    },
+                    IsMaterialRegistered = true,
+                    StatusUpdatedDate = new DateTime(2020, 01, 01, 12, 00, 00)
+                }
+            }
         };
 
         var expected = new RegistrationTaskOverviewDto
         {
             Id = externalId,
-            Regulator = "EA"
+            Regulator = "EA",
+            Materials = new List<RegistrationMaterialDto>
+            {
+                new ()
+                {
+                    MaterialName = "Steel",
+                    IsMaterialRegistered = true,
+                    StatusUpdatedDate = new DateTime(2020, 01, 01, 12, 00, 00),
+                    Comments = ""
+                }
+            }
 
         };
 
