3bd502901b2dc559278a70691168b13560e431fa
Harikishnan Korasseril (ext) <harikishnan.korasseril.external@eviden.com>
2025-07-02 09:36:31 +0100

    Correcting the Sonar

--- src/EPR.PRN.Backend.API/Commands/RegistrationReprocessingIOCommand.cs
+++ src/EPR.PRN.Backend.API/Commands/RegistrationReprocessingIOCommand.cs
@@ -2,35 +2,7 @@
 
 namespace EPR.PRN.Backend.Data.DTO;
 
-public class RegistrationReprocessingIOCommand : IRequest
+public class RegistrationReprocessingIOCommand : RegistrationReprocessingIORequestDto,IRequest
 {
-    public int Id { get; set; }
-
-    public Guid ExternalId { get; set; }
-
-    public int RegistrationMaterialId { get; set; }
-
-    public bool ReprocessingPackagingWasteLastYearFlag { get; set; }
-
-    public decimal UKPackagingWasteTonne { get; set; }
-
-    public decimal NonUKPackagingWasteTonne { get; set; }
-
-    public decimal NotPackingWasteTonne { get; set; }
-
-    public decimal SenttoOtherSiteTonne { get; set; }
-
-    public decimal ContaminantsTonne { get; set; }
-
-    public decimal ProcessLossTonne { get; set; }
-
-    public string? PlantEquipmentUsed { get; set; }
-
-    public decimal TotalInputs { get; set; }
-
-    public decimal TotalOutputs { get; set; }
-
-    public string? TypeOfSuppliers { get; set; }
-
-    public List<RegistrationReprocessingIORawMaterialOrProductsDto> RegistrationReprocessingIORawMaterialOrProducts { get; set; } = [];
+    
 }
