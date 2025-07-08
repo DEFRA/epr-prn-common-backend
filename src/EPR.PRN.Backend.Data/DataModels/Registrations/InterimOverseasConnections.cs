2cbcad73552caf00663295171cc86d841f454582
Adrian Clare <adrian.clare.external@eviden.com>
2025-07-08 11:28:22 +0100

    exclude model from code coverage

--- src/EPR.PRN.Backend.Data/DataModels/Registrations/InterimOverseasConnections.cs
+++ src/EPR.PRN.Backend.Data/DataModels/Registrations/InterimOverseasConnections.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EPR.PRN.Backend.Data.DataModels.Registrations
 {
+    // Currently this recrods fromd this table are deleted from cascade delete, feature not implemented for adding records.
+    [ExcludeFromCodeCoverage]
     [Table("Public.InterimOverseasConnections")]
     public class InterimOverseasConnections
     {
