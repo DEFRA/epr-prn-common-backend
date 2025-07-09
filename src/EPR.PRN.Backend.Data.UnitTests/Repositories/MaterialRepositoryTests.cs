07d0cd63b55be9f327e36ef2ef09badedb042273
Gurpal Kahlon <gurpal.kahlon.external@eviden.com>
2025-07-09 10:14:00 +0100

    Feature/559759 do you use interim sites (#314)
    * feature/538787 select package waste you will reprocess (#297)
    
    * feature/538787 select package waste you will reprocess
    
    * Added migration for new Lookup.RegulatorTask entry.  Added new task name.  Generated new migrations script
    
    * Adde new applicantTaskStatus endpoint and tests
    
    * Feature/563047 ex registration   overseas reprocessor details (#299)
    
    * Get Countries Lookup
    
    * Added All World Countries
    
    * Updated Countries
    
    * Fix Build
    
    * Updates for SonarQube
    
    * Added Migration
    
    * Removed last migration as this is not necessary now following updated code from other team related to task tables
    
    * Updates to align with work from other team.  Updated tests.
    
    * Adding migration to update the column name -> TypeOfSuppliers (#298)
    
    * Adding migration to update the column name -> TypeOfSuppliers
    Updated SeedData 4&13
    
    * Correcting the migration script
    
    * Correcting migration
    
    * Correcting the migration script
    
    * Feature/549006: Added registration material contact table (#302)
    
    * Added RegistrationMaterialContact and upsert endpoint.
    
    * Updated migrations.
    
    * Added unit tests.
    
    * Undo migrations.
    
    * Add migration
    
    * Update migrations.
    
    * Added tests
    
    * Added tests
    
    * Feature/549006: Add registration material contact. (#309)
    
    * Added RegistrationMaterialContact and upsert endpoint.
    
    * Updated migrations.
    
    * Added unit tests.
    
    * Undo migrations.
    
    * Add migration
    
    * Update migrations.
    
    * Add unit tests.
    
    * Feature/559759 do you use interim sites (#307)
    
    * Added migration for new Lookup.RegulatorTask entry.  Added new task name.  Generated new migrations script
    
    * Adde new applicantTaskStatus endpoint and tests
    
    * Removed last migration as this is not necessary now following updated code from other team related to task tables
    
    * Updates to align with work from other team.  Updated tests.
    
    * Added tests
    
    * Added tests
    
    * Ran in release mode to generate migrations.sql
    
    * Updated broken tests
    
    * Updated interface to match implementation parameter names
    
    * Feature/543688 re registration apply pay fees reprocessing inputs and outputs (#312)
    
    * Adding migration to update the column name -> TypeOfSuppliers
    Updated SeedData 4&13
    
    * Correcting the migration script
    
    * Bank end changes RegistrationReprocessingIORawMaterial
    
    * Correcting migration
    
    * Correcting the migration script
    
    * Backend changes
    
    * Correcting the backend
    
    * Reveerting the unwanted changes
    
    * Adding the tests
    
    * Correcting the Sonar
    
    * Clean up
    
    * Adding the vallidation
    
    * feature/543688 fixed some error
    
    * Correcting the tests
    
    * Adding the repo tests
    
    ---------
    
    Co-authored-by: Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
    
    * Renamed applicant to application
    
    * Feature/fixing migration issue (#317)
    
    * fixing migration issue
    
    ---------
    
    Co-authored-by: Zozan KIRKUTAY <167984276+zozanKirkutay@users.noreply.github.com>
    Co-authored-by: Mark Forster <160658973+MW-Forster@users.noreply.github.com>
    Co-authored-by: Paul Shimmen <paul.shimmen@eviden.com>
    Co-authored-by: hariEviden <harikishnan.korasseril.external@eviden.com>
    Co-authored-by: Tim Jones <tim.jones.external@eviden.com>
    Co-authored-by: mw-forster <mark.forster@eviden.com>
    Co-authored-by: Zozan Kirkutay <zozan.kirkutay.external@eviden.com>

--- src/EPR.PRN.Backend.Data.UnitTests/Repositories/MaterialRepositoryTests.cs
+++ src/EPR.PRN.Backend.Data.UnitTests/Repositories/MaterialRepositoryTests.cs
@@ -1,10 +1,12 @@
 ﻿using System.Xml.XPath;
 using EPR.PRN.Backend.API.Common.Enums;
+using EPR.PRN.Backend.API.Common.Exceptions;
 using EPR.PRN.Backend.Data.DataModels;
 using EPR.PRN.Backend.Data.DataModels.Registrations;
 using EPR.PRN.Backend.Data.Repositories;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.Testing.Platform.Extensions;
 using Moq;
 using Moq.EntityFrameworkCore;
@@ -18,30 +20,41 @@ public class MaterialRepositoryTests
     private Mock<EprContext> _mockEprContext;
     private static Guid _registrationId = Guid.NewGuid();
     private readonly List<Material> _materials =
-        [
-            new Material { Id = 1, MaterialCode = "PL", MaterialName = MaterialType.Plastic.ToString() },
-            new Material { Id = 2, MaterialCode = "WD", MaterialName = MaterialType.Wood.ToString() },
-            new Material { Id = 3, MaterialCode = "AL", MaterialName = MaterialType.Aluminium.ToString() },
-            new Material { Id = 4, MaterialCode = "ST", MaterialName = MaterialType.Steel.ToString() },
-            new Material { Id = 5, MaterialCode = "PC", MaterialName = MaterialType.Paper.ToString() },
-            new Material { Id = 6, MaterialCode = "GL", MaterialName = MaterialType.Glass.ToString() },
-            new Material { Id = 7, MaterialCode = "GR", MaterialName = MaterialType.GlassRemelt.ToString() },
-            new Material { Id = 8, MaterialCode = "FC", MaterialName = MaterialType.FibreComposite.ToString() }
-        ];
+		[
+			new Material { Id = 1, MaterialCode = "PL", MaterialName = MaterialType.Plastic.ToString() },
+			new Material { Id = 2, MaterialCode = "WD", MaterialName = MaterialType.Wood.ToString() },
+			new Material { Id = 3, MaterialCode = "AL", MaterialName = MaterialType.Aluminium.ToString() },
+			new Material { Id = 4, MaterialCode = "ST", MaterialName = MaterialType.Steel.ToString() },
+			new Material { Id = 5, MaterialCode = "PC", MaterialName = MaterialType.Paper.ToString() },
+			new Material { Id = 6, MaterialCode = "GL", MaterialName = MaterialType.Glass.ToString() },
+			new Material { Id = 7, MaterialCode = "GR", MaterialName = MaterialType.GlassRemelt.ToString() },
+			new Material { Id = 8, MaterialCode = "FC", MaterialName = MaterialType.FibreComposite.ToString() }
+		];
 
     private readonly List<RegistrationMaterial> _registrationMaterials =
         [
             new RegistrationMaterial { Id = 1, RegistrationId = 1, MaterialId = 1, IsMaterialRegistered = true, Registration = new Registration() { ExternalId = _registrationId } },
         ];
 
-    [TestInitialize]
-    public void Setup()
-    {
-        var dbContextOptions = new DbContextOptionsBuilder<EprContext>().Options;
-        _mockEprContext = new Mock<EprContext>(dbContextOptions);
-        _mockEprContext.Setup(context => context.Material).ReturnsDbSet(_materials);
+    private MaterialRepository _materialRepositoryFull;
+    private Mock<ILogger<MaterialRepository>> _mockLogger;
+    private EprContext _context;
+	[TestInitialize]
+	public void Setup()
+	{
+		var dbContextOptions = new DbContextOptionsBuilder<EprContext>().Options;
+		_mockEprContext = new Mock<EprContext>(dbContextOptions);
+		_mockEprContext.Setup(context => context.Material).ReturnsDbSet(_materials);
         _mockEprContext.Setup(context => context.RegistrationMaterials).ReturnsDbSet(_registrationMaterials);
-        _materialRepository = new MaterialRepository(_mockEprContext.Object);
+        _mockLogger = new Mock<ILogger<MaterialRepository>>();
+        _materialRepository = new MaterialRepository(_mockEprContext.Object, _mockLogger.Object);
+
+        var options = new DbContextOptionsBuilder<EprContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new EprContext(options);
+        _materialRepositoryFull = new MaterialRepository(_context, _mockLogger.Object);
     }
 
     [TestMethod]
@@ -412,6 +425,104 @@ public class MaterialRepositoryTests
         existingIO.TotalInputs.Should().Be(20);
         existingIO.TotalOutputs.Should().Be(18);
         existingIO.PlantEquipmentUsed.Should().Be("Updated Machine");
+    }
+
+    [TestMethod]
+    public async Task UpdateApplicationRegistrationTaskStatusAsync_ShouldAddNewStatus_WhenNotExists()
+    {
+        // Arrange
+        var registrationMaterialId = Guid.NewGuid();
+        var registrationMaterial = new RegistrationMaterial { Id = 1, ExternalId = registrationMaterialId, RegistrationId = 1 };
+        var registration = new Registration { Id = 1, ApplicationTypeId = 1, ExternalId = Guid.NewGuid() };
+        var task = new LookupApplicantRegistrationTask { Name = "NewTask", ApplicationTypeId = 1, IsMaterialSpecific = true };
+        var status = new LookupTaskStatus { Name = nameof(TaskStatuses.Completed) };
+
+        await _context.Registrations.AddAsync(registration);
+        await _context.RegistrationMaterials.AddAsync(registrationMaterial);
+        await _context.LookupApplicantRegistrationTasks.AddAsync(task);
+        await _context.LookupTaskStatuses.AddAsync(status);
+        await _context.SaveChangesAsync();
+
+        // Act
+        await _materialRepositoryFull.UpdateApplicationRegistrationTaskStatusAsync("NewTask", registrationMaterialId, TaskStatuses.Completed);
+
+        // Assert
+        var result = await _materialRepositoryFull.GetTaskStatusAsync("NewTask", registrationMaterialId);
+        result.Should().NotBeNull();
+        result!.TaskStatus.Name.Should().Be(TaskStatuses.Completed.ToString());
+    }
+
+    [TestMethod]
+    public async Task UpdateApplicationRegistrationTaskStatusAsync_ShouldUpdateExistingStatus()
+    {
+        // Arrange
+        var registrationMaterialId = Guid.NewGuid();
+        var registrationMaterial = new RegistrationMaterial { Id = 1, ExternalId = registrationMaterialId, RegistrationId = 1 };
+        var registration = new Registration { Id = 1, ApplicationTypeId = 1, ExternalId = Guid.NewGuid() };
+        var task = new LookupApplicantRegistrationTask { Name = "ExistingTask", ApplicationTypeId = 1, IsMaterialSpecific = false };
+        var oldStatus = new LookupTaskStatus { Name = TaskStatuses.Started.ToString() };
+        var newStatus = new LookupTaskStatus { Name = TaskStatuses.Completed.ToString() };
+
+        await _context.Registrations.AddAsync(registration);
+        await _context.RegistrationMaterials.AddAsync(registrationMaterial);
+        await _context.LookupApplicantRegistrationTasks.AddAsync(task);
+        await _context.LookupTaskStatuses.AddRangeAsync(oldStatus, newStatus);
+
+        var taskStatus = new ApplicantRegistrationTaskStatus
+        {
+            RegistrationId = 2,
+            Task = task,
+            TaskStatus = oldStatus,
+            RegistrationMaterialId = registrationMaterial.Id,
+        };
+
+        await _context.RegistrationTaskStatus.AddAsync(taskStatus);
+        await _context.SaveChangesAsync();
+
+        // Act
+        await _materialRepositoryFull.UpdateApplicationRegistrationTaskStatusAsync("ExistingTask", registrationMaterialId, TaskStatuses.Completed);
+
+        // Assert
+        var result = await _materialRepositoryFull.GetTaskStatusAsync("ExistingTask", registrationMaterialId);
+        result!.TaskStatus.Name.Should().Be(TaskStatuses.Completed.ToString());
+    }
+
+    [TestMethod]
+    public async Task UpdateApplicationRegistrationTaskStatusAsync_ShouldThrow_WhenRegistrationNotFound()
+    {
+        // Arrange
+        var registrationId = Guid.NewGuid();
+        var status = new LookupTaskStatus { Name = TaskStatuses.Completed.ToString() };
+        await _context.LookupTaskStatuses.AddAsync(status);
+        await _context.SaveChangesAsync();
+
+        // Act
+        Func<Task> act = async () => await _materialRepositoryFull.UpdateApplicationRegistrationTaskStatusAsync("AnyTask", registrationId, TaskStatuses.Completed);
+
+        // Assert
+        await act.Should().ThrowAsync<KeyNotFoundException>();
+    }
+
+    [TestMethod]
+    public async Task UpdateApplicationRegistrationTaskStatusAsync_ShouldThrow_WhenTaskNotFound()
+    {
+        // Arrange
+        var registrationMaterialId = Guid.NewGuid();
+        var registrationMaterial = new RegistrationMaterial { Id = 1, ExternalId = registrationMaterialId, RegistrationId = 1 };
+        var registration = new Registration { Id = 1, ApplicationTypeId = 1, ExternalId = Guid.NewGuid() };
+        var status = new LookupTaskStatus { Name = TaskStatuses.Completed.ToString() };
+
+        await _context.Registrations.AddAsync(registration);
+        await _context.RegistrationMaterials.AddAsync(registrationMaterial);
+        await _context.LookupTaskStatuses.AddAsync(status);
+        await _context.SaveChangesAsync();
+
+        // Act
+        Func<Task> act = async () => await _materialRepositoryFull.UpdateApplicationRegistrationTaskStatusAsync("MissingTask", registrationMaterialId, TaskStatuses.Completed);
+
+        // Assert
+        await act.Should().ThrowAsync<RegulatorInvalidOperationException>()
+            .WithMessage("No Valid Task Exists: MissingTask");
     }
 
     [TestMethod]
--- src/EPR.PRN.Backend.Data/Interfaces/IMaterialRepository.cs
+++ src/EPR.PRN.Backend.Data/Interfaces/IMaterialRepository.cs
@@ -1,4 +1,5 @@
-﻿using EPR.PRN.Backend.Data.DataModels;
+﻿using EPR.PRN.Backend.API.Common.Enums;
+using EPR.PRN.Backend.Data.DataModels;
 using EPR.PRN.Backend.Data.DataModels.Registrations;
 using EPR.PRN.Backend.Data.DTO;
 
@@ -9,6 +10,7 @@ public interface IMaterialRepository
     Task<IEnumerable<Material>> GetAllMaterials();
 
     Task<RegistrationMaterialContact> UpsertRegistrationMaterialContact(Guid registrationMaterialId, Guid userId);
+    Task UpdateApplicationRegistrationTaskStatusAsync(string taskName, Guid registrationMaterialId, TaskStatuses status);
 
     Task UpsertRegistrationReprocessingDetailsAsync(Guid registrationMaterialId, RegistrationReprocessingIO registrationReprocessingIO);
 
