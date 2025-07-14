95328c4a444a42c179aab2ad2679fc11eba0c715
DavidTonthat <104467315+davidtonthat@users.noreply.github.com>
2025-07-14 09:31:24 +0100

    Feature/572609 check this site fulfills these conditions - Backend (#325)
    * Extended AccreditationDto.cs, AccreditationRequestDto.cs to match their counterparts in epr-prn-rep-exp-frontend.
    
    * Extended AccreditationEntity to match the structure of AccreditationDto.cs
    
    * Dummy change
    
    * 1.Added Accreditation entity classes MeetConditionsOfExport.cs, SiteCheckStatus.cs, OverseasAccreditationSite.cs and updated Accreditation.cs, EprAccreditationContext.cs 2.Added OverseasAccreditationSiteRepository.cs
    
    * Added OverseasAccreditationSiteDto.cs & OverseasAccreditationSiteService.cs for database access.
    
    * Added OverseasAccreditationSiteController.cs
    
    * Restored original AccreditationDto.cs, AccreditationRequestDto.cs
    
    * 1.Updated AccreditationProfile.cs to handle mapping between entity OverseasAccreditationSite & OverseasAccreditationSiteDto. 2. Updated OverseasAccreditationSiteRepository.cs for when there's no Accreditation in a local database
    
    * Added unit tests OverseasAccreditationSiteRepositoryTests.cs
    
    * Added unit tests OverseasAccreditationSiteServiceTests.cs
    
    * Added unit tests OverseasAccreditationSiteControllerTests.cs
    
    ---------
    
    Co-authored-by: Dat Tonthat (ext) <dat.tonthat.external@atos.net>

---
+++ src/EPR.PRN.Backend.API.UnitTests/Controllers/OverseasAccreditationSiteControllerTests.cs
@@ -0,0 +1,80 @@
+using EPR.PRN.Backend.API.Controllers;
+using EPR.PRN.Backend.API.Dto.Accreditation;
+using EPR.PRN.Backend.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using Moq;
+
+namespace EPR.PRN.Backend.API.UnitTests.Controllers;
+
+[TestClass]
+public class OverseasAccreditationSiteControllerTests
+{
+    private Mock<IOverseasAccreditationSiteService> _serviceMock;
+    private OverseasAccreditationSiteController _controller;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _serviceMock = new Mock<IOverseasAccreditationSiteService>();
+        _controller = new OverseasAccreditationSiteController(_serviceMock.Object);
+    }
+
+    [TestMethod]
+    public async Task GetAllByAccreditationId_ShouldReturnSiteDtos_WhenItemsExist()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var siteDtos = new List<OverseasAccreditationSiteDto>
+        {
+            new OverseasAccreditationSiteDto {
+                ExternalId = accreditationId, OrganisationName = "Hun Manet Recycler Ltd", MeetConditionsOfExportId = 1, SiteCheckStatusId = 2
+            }
+        };
+        _serviceMock.Setup(s => s.GetAllByAccreditationId(accreditationId)).ReturnsAsync(siteDtos);
+
+        // Act
+        var result = await _controller.GetAllByAccreditationId(accreditationId);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        var sitesResult = result as OkObjectResult;
+        sitesResult.Value.Should().BeEquivalentTo(siteDtos);
+        _serviceMock.Verify(s => s.GetAllByAccreditationId(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task GetAllByAccreditationId_ShouldReturnEmptyList_WhenNoItems()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var siteDtos = new List<OverseasAccreditationSiteDto>();
+        _serviceMock.Setup(s => s.GetAllByAccreditationId(accreditationId)).ReturnsAsync(siteDtos);
+
+        // Act
+        var result = await _controller.GetAllByAccreditationId(accreditationId);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+        var sitesResult = result as OkObjectResult;
+        sitesResult.Value.Should().BeEquivalentTo(siteDtos);
+        _serviceMock.Verify(s => s.GetAllByAccreditationId(accreditationId), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task Post_ShouldReturnNoContent()
+    {
+        // Arrange
+        var accreditationId = Guid.NewGuid();
+        var request = new OverseasAccreditationSiteDto {
+            ExternalId = accreditationId, OrganisationName = "Hun Manet Recycler Ltd", MeetConditionsOfExportId = 2, SiteCheckStatusId = 2
+        };
+
+        // Act
+        var result = await _controller.Post(accreditationId, request);
+
+        // Assert
+        result.Should().BeOfType<NoContentResult>();
+        _serviceMock.Verify(s => s.PostByAccreditationId(accreditationId, request), Times.Once);
+    }
+}
