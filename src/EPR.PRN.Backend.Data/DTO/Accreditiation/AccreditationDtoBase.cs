2effb21bfc0ddecb535907f3f3b31bb67794edfd
Sushil Likhite (Eviden) <sushil.likhite@Defra.onmicrosoft.com>
2025-07-15 16:28:18 +0100

    Reduce properties

--- src/EPR.PRN.Backend.Data/DTO/Accreditiation/AccreditationDtoBase.cs
+++ src/EPR.PRN.Backend.Data/DTO/Accreditiation/AccreditationDtoBase.cs
@@ -16,22 +16,6 @@ namespace EPR.PRN.Backend.Data.DTO.Accreditiation
         public string? AccreferenceNumber { get; set; }
 
         public int? AccreditationYear { get; set; }
-        public int? PrnTonnage { get; set; }
-        public bool PrnTonnageAndAuthoritiesConfirmed { get; set; }
-        public decimal? InfrastructurePercentage { get; set; }
-        public decimal? PackagingWastePercentage { get; set; }
-        public decimal? BusinessCollectionsPercentage { get; set; }
-        public decimal? NewUsesPercentage { get; set; }
-        public decimal? NewMarketsPercentage { get; set; }
-        public decimal? CommunicationsPercentage { get; set; }
-        public decimal? OtherPercentage { get; set; }
-        public string? InfrastructureNotes { get; set; }
-        public string? PackagingWasteNotes { get; set; }
-        public string? BusinessCollectionsNotes { get; set; }
-        public string? NewUsesNotes { get; set; }
-        public string? NewMarketsNotes { get; set; }
-        public string? OtherNotes { get; set; }
-        public bool BusinessPlanConfirmed { get; set; }
         public Guid CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public Guid UpdatedBy { get; set; }
