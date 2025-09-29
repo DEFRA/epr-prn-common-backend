d3b2743ba8c59a1132c03b0374d60d7877fcef14
jayakrishnanappusamy <jayakrishnan.appusamy@atos.net>
2025-09-29 16:48:02 +0100

    remove ambiguous constructor in eprcontext (#359)
    * Removed the ambiguous contructor that DI is falling to recognize

--- src/EPR.PRN.Backend.Data/EprContext.cs
+++ src/EPR.PRN.Backend.Data/EprContext.cs
@@ -18,10 +18,6 @@ public class EprContext : DbContext
     {
 
     }
-    public EprContext(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
 
     public EprContext(DbContextOptions<EprContext> options)
         : base(options)
