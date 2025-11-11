3f27ff6e353d3ecd784e5a03823f3db45f163e6c
Tim Peel <tim.peel@equalexperts.com>
2025-11-11 15:48:08 +0000

    Allow DB creation and seeding if specified

--- src/EPR.PRN.Backend.Data/Scripts/run-migrations.sh
+++ src/EPR.PRN.Backend.Data/Scripts/run-migrations.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+/opt/mssql-tools/bin/sqlcmd -S $SERVER,$PORT -U $USER -P $PASSWORD -Q "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'$DATABASE') CREATE DATABASE [$DATABASE]" -I
+
 if [[ -s $1 ]]; then
   /opt/mssql-tools/bin/sqlcmd -S $SERVER,$PORT -U $USER -P $PASSWORD -d $DATABASE -i "$1" -I
 else
   echo The file "$1" is empty. No update has been triggered.
+fi
+
+if [[ -s $2 ]]; then
+  /opt/mssql-tools/bin/sqlcmd -S $SERVER,$PORT -U $USER -P $PASSWORD -d $DATABASE -i "$2" -I
 fi
