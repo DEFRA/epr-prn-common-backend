8c3d41c9b72fffdcc985a63f79f5a5f0a695e881
Jayakrishnan Appusamy <jayakrishnan.appusamy@atos.net>
2025-11-19 15:35:46 +0000

    Fixed decimal precision while calculating material obligation

--- src/EPR.PRN.Backend.Obligation/Services/MaterialCalculationService.cs
+++ src/EPR.PRN.Backend.Obligation/Services/MaterialCalculationService.cs
@@ -6,13 +6,14 @@ namespace EPR.PRN.Backend.Obligation.Services
     {
         public int Calculate(double target, int tonnage)
         {
-            return (int)Math.Ceiling(target * tonnage);
-        }
+            var result = (decimal)target * tonnage;
+			return (int)Math.Ceiling(result);
+		}
 
-        public (int remelt, int remainder) CalculateGlass(double target, double remeltTarget, int tonnage)
+		public (int remelt, int remainder) CalculateGlass(double target, double remeltTarget, int tonnage)
         {
-            var initialTarget = target * tonnage;
-            var glassRemelt = (int)Math.Ceiling(remeltTarget * initialTarget);
+            var initialTarget = (decimal)target * tonnage;
+            var glassRemelt = (int)Math.Ceiling((decimal)remeltTarget * initialTarget);
             var glassTotal = (int)Math.Ceiling(initialTarget);
             var glassOther = glassTotal - glassRemelt;
 
