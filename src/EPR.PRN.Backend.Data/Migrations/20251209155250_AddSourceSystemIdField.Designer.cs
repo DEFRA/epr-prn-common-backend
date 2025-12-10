f85f9f45f430443f8f86a5f4b0f18cda69b82291
Tim Rutter <tim.rutter@devsectr.com>
2025-12-10 15:33:07 +0000

    Use 8.0.8

--- src/EPR.PRN.Backend.Data/Migrations/20251209155250_AddSourceSystemIdField.Designer.cs
+++ src/EPR.PRN.Backend.Data/Migrations/20251209155250_AddSourceSystemIdField.Designer.cs
@@ -20,7 +20,7 @@ namespace EPR.PRN.Backend.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.20")
+                .HasAnnotation("ProductVersion", "8.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
