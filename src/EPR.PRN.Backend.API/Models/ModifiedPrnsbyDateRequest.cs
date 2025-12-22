c889ace46fc1a9ebefaef2a77a58e95cb8e42521
Tim Rutter <51402860+timrutter@users.noreply.github.com>
2025-12-22 16:08:49 +0000

    REEX-145 format files in preparation of changes (#386)
    * Remove sourcesystemid from old endpoint
    
    * Remove further code
    
    * Format
    
    * Interim
    
    * Remove unneeded line
    
    * Interim
    
    * Format
    
    * Add tests
    
    * Added tests
    
    * Improve approach and unit tests
    
    * Log truncation
    
    * Add identifiers
    
    * Tidy up file structure
    
    * Fix tests
    
    * Fix tests
    
    * Allow valid prns through
    
    * Merge and fix tests
    
    * fix further test
    
    * Format various files

--- src/EPR.PRN.Backend.API/Models/ModifiedPrnsbyDateRequest.cs
+++ src/EPR.PRN.Backend.API/Models/ModifiedPrnsbyDateRequest.cs
@@ -8,7 +8,6 @@ public class ModifiedPrnsbyDateRequest : IValidatableObject
 {
     [Required(ErrorMessage = "From date is required.")]
     [ModelBinder(BinderType = typeof(DateTimeModelBinder))]
-
     public DateTime From { get; set; }
 
     [Required(ErrorMessage = "To date is required.")]
@@ -19,17 +18,26 @@ public class ModifiedPrnsbyDateRequest : IValidatableObject
     {
         if (From == DateTime.MinValue || From == DateTime.MaxValue || From == default(DateTime))
         {
-            yield return new ValidationResult("From date must be a valid date and not the minimum, maximum, or default date.", new[] { nameof(From) });
+            yield return new ValidationResult(
+                "From date must be a valid date and not the minimum, maximum, or default date.",
+                new[] { nameof(From) }
+            );
         }
 
         if (To == DateTime.MinValue || To == DateTime.MaxValue || To == default(DateTime))
         {
-            yield return new ValidationResult("To date must be a valid date and not the minimum, maximum, or default date.", new[] { nameof(To) });
+            yield return new ValidationResult(
+                "To date must be a valid date and not the minimum, maximum, or default date.",
+                new[] { nameof(To) }
+            );
         }
 
         if (To < From)
         {
-            yield return new ValidationResult("The 'To' date must not be earlier than the 'From' date.", new[] { nameof(To) });
+            yield return new ValidationResult(
+                "The 'To' date must not be earlier than the 'From' date.",
+                new[] { nameof(To) }
+            );
         }
     }
 }
