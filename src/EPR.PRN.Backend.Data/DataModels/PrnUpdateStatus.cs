5ff0bc266c26619b1aed9f013ec3509a1827952e
Tim Rutter <tim.rutter@devsectr.com>
2026-01-19 14:30:16 +0000

    nullable

--- src/EPR.PRN.Backend.Data/DataModels/PrnUpdateStatus.cs
+++ src/EPR.PRN.Backend.Data/DataModels/PrnUpdateStatus.cs
@@ -6,6 +6,6 @@
         public required string EvidenceStatusCode { get; set; }
         public DateTime? StatusDate { get; set; }
         public required string AccreditationYear { get; set; }
-        public required string ObligationYear { get; set; }
+        public string? ObligationYear { get; set; }
     }
 }
