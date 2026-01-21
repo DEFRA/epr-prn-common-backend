b26848983500ceae50c0c3e0af44ac4c2f1d51e9
Tim Peel <tim.peel@equalexperts.com>
2026-01-21 10:27:46 +0000

    Support December waste when retrieving PRNs internally via year (#394)
    * Query formatting and simplification
    
    * Allow PRN retrieval by year to include December waste if the year requested is 2026
    
    * Allow the December waste logic to span future years rather than be fixed on 2026 only

--- src/EPR.PRN.Backend.Data.UnitTests/Repositories/PrnRepositoryTests.cs
+++ src/EPR.PRN.Backend.Data.UnitTests/Repositories/PrnRepositoryTests.cs
@@ -33,16 +33,16 @@ namespace EPR.PRN.Backend.Data.UnitTests.Repositories
             var fixture = new Fixture();
             var prns = fixture.CreateMany<Eprn>(10).ToList();
             prns[0].OrganisationId = organisationId;
-            prns[0].PrnStatusId = 1; // ACCEPTED
+            prns[0].PrnStatusId = (int)EprnStatus.ACCEPTED;
             prns[0].ObligationYear = year.ToString();
             prns[1].OrganisationId = organisationId;
-            prns[1].PrnStatusId = 2; // AWAITINGACCEPTANCE
+            prns[1].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
             prns[1].ObligationYear = year.ToString();
             await _context.Prn.AddRangeAsync(prns, CancellationToken.None);
             var prnStatuses = new[]
             {
-            new PrnStatus { Id = 1, StatusName = EprnStatus.ACCEPTED.ToString() },
-            new PrnStatus { Id = 2, StatusName = EprnStatus.AWAITINGACCEPTANCE.ToString() }
+            new PrnStatus { Id = (int)EprnStatus.ACCEPTED, StatusName = EprnStatus.ACCEPTED.ToString() },
+            new PrnStatus { Id = (int)EprnStatus.AWAITINGACCEPTANCE, StatusName = EprnStatus.AWAITINGACCEPTANCE.ToString() }
             };
             await _context.PrnStatus.AddRangeAsync(prnStatuses, CancellationToken.None);
             await _context.SaveChangesAsync(CancellationToken.None);
@@ -54,6 +54,60 @@ namespace EPR.PRN.Backend.Data.UnitTests.Repositories
             result.Should().HaveCount(2);
             result.Should().ContainSingle(r => r.Status.StatusName == EprnStatus.ACCEPTED.ToString());
             result.Should().ContainSingle(r => r.Status.StatusName == EprnStatus.AWAITINGACCEPTANCE.ToString());
+        }
+
+        [TestMethod]
+        public async Task GetAcceptedAndAwaitingPrnsByYearAsync_WhenDecemberWaste_ReturnsFilteredPrns()
+        {
+            // Arrange
+            var organisationId = Guid.NewGuid();
+            var year = 2026;
+            var fixture = new Fixture();
+            var prns = fixture.CreateMany<Eprn>(10).ToList();
+            prns[0].OrganisationId = organisationId;
+            prns[0].PrnStatusId = (int)EprnStatus.ACCEPTED;
+            prns[0].ObligationYear = year.ToString();
+            prns[1].OrganisationId = organisationId;
+            prns[1].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
+            prns[1].ObligationYear = year.ToString();
+            prns[2].OrganisationId = organisationId;
+            prns[2].PrnStatusId = (int)EprnStatus.ACCEPTED;
+            prns[2].ObligationYear = "2025";
+            prns[2].DecemberWaste = true;
+            prns[3].OrganisationId = organisationId;
+            prns[3].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
+            prns[3].ObligationYear = "2025";
+            prns[3].DecemberWaste = true;
+            prns[4].OrganisationId = organisationId;
+            prns[4].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
+            prns[4].ObligationYear = "2025";
+            prns[4].DecemberWaste = false;
+            prns[5].OrganisationId = organisationId;
+            prns[5].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
+            prns[5].ObligationYear = "2027";
+            prns[5].DecemberWaste = true;
+            prns[6].OrganisationId = organisationId;
+            prns[6].PrnStatusId = (int)EprnStatus.AWAITINGACCEPTANCE;
+            prns[6].ObligationYear = "2027";
+            prns[6].DecemberWaste = false;
+            await _context.Prn.AddRangeAsync(prns, CancellationToken.None);
+            var prnStatuses = new[]
+            {
+                new PrnStatus { Id = (int)EprnStatus.ACCEPTED, StatusName = nameof(EprnStatus.ACCEPTED) },
+                new PrnStatus { Id = (int)EprnStatus.AWAITINGACCEPTANCE, StatusName = nameof(EprnStatus.AWAITINGACCEPTANCE) }
+            };
+            await _context.PrnStatus.AddRangeAsync(prnStatuses, CancellationToken.None);
+            await _context.SaveChangesAsync(CancellationToken.None);
+
+            // Act
+            var result = _repository.GetAcceptedAndAwaitingPrnsByYear(organisationId, year).ToList();
+
+            // Assert
+            result.Should().HaveCount(4);
+            result.Should().ContainSingle(r => r.Status.StatusName == nameof(EprnStatus.ACCEPTED) && r.Eprn.ObligationYear == year.ToString());
+            result.Should().ContainSingle(r => r.Status.StatusName == nameof(EprnStatus.AWAITINGACCEPTANCE) && r.Eprn.ObligationYear == year.ToString());
+            result.Should().ContainSingle(r => r.Status.StatusName == nameof(EprnStatus.ACCEPTED) && r.Eprn.ObligationYear == "2025");
+            result.Should().ContainSingle(r => r.Status.StatusName == nameof(EprnStatus.AWAITINGACCEPTANCE) && r.Eprn.ObligationYear == "2025");
         }
     }
 }
