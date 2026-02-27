f8bbd3d49ffe1d114257cf2936ec3349e837e9e4
Tim Peel <tim.peel@equalexperts.com>
2026-02-27 09:51:31 +0000

    Add PrnMaterialMapping for Fibre so PRNs can be found and wrapped within Paper calculations

---
+++ src/EPR.PRN.Backend.Data/Migrations/20260226162853_AddFibreMaterialMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.PRN.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFibreMaterialMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PrnMaterialMapping",
+                columns: ["Id", "NPWDMaterialName", "PRNMaterialId"],
+                values: new object[,]
+                {
+                    { 10, "Fibre", 8 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PrnMaterialMapping",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
