1ef6b410c36c54c478935734ad238282df9b4314
Tim Peel <tim.peel@equalexperts.com>
2026-04-10 10:15:16 +0100

    MO-51: Add index for obligation calculations lookup (#403)

---
+++ src/EPR.PRN.Backend.Data/Migrations/20260319122432_AddObligationCalculationIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.PRN.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddObligationCalculationIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ObligationCalculations_Calculate",
+                table: "ObligationCalculations",
+                columns: ["Year", "IsDeleted", "SubmitterId"]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ObligationCalculations_Calculate",
+                table: "ObligationCalculations");
+        }
+    }
+}
