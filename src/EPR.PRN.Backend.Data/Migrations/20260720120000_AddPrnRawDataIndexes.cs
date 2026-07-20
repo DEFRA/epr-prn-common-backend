4802c389fa8b0c63e5efa36e52fd0bf1b24719cf
Tim Peel <tim.peel@equalexperts.com>
2026-07-20 16:49:42 +0100

    Add migration for new indexes

---
+++ src/EPR.PRN.Backend.Data/Migrations/20260720120000_AddPrnRawDataIndexes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.PRN.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(EprContext))]
+    [Migration("20260720120000_AddPrnRawDataIndexes")]
+    public partial class AddPrnRawDataIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Prn_SourceSystemId_Id",
+                table: "Prn",
+                columns: ["SourceSystemId", "Id"]);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrnStatusHistory_PrnIdFk_CreatedOn_Id",
+                table: "PrnStatusHistory",
+                columns: ["PrnIdFk", "CreatedOn", "Id"]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Prn_SourceSystemId_Id",
+                table: "Prn");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PrnStatusHistory_PrnIdFk_CreatedOn_Id",
+                table: "PrnStatusHistory");
+        }
+    }
+}
